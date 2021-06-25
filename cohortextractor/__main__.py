87304b471cde27b7ad2ede845faf7641aa04f72e
Ben Butler-Cole <ben@bridesmere.com>
2021-06-25 12:51:54 +0100

    Actually load a study definition and convert it to a SQL query
    There is lots of stuff that only works because it's hard-coded and the
    conversion to SQL is hopelessly naïve. But it kind of works.

--- cohortextractor/__main__.py
+++ cohortextractor/__main__.py
@@ -1,7 +1,52 @@
+import importlib
+import sys
+import time
 from pathlib import Path
+
+import pymssql
+from pymssql import OperationalError
+
+
+def get_class_vars(cls):
+    default_vars = set(dir(type("ArbitraryEmptyClass", (), {})))
+    return [(key, value) for key, value in vars(cls).items() if key not in default_vars]
 
 
 if __name__ == "__main__":
+    sys.path.append("/workspace")
+    study_definition = importlib.import_module("study_definition")
+    Cohort = getattr(study_definition, "Cohort")
+
+    cohort = {key: value for key, value in get_class_vars(Cohort)}
+
+    query = cohort["everything"]
+    sql = f"SELECT patient_id, {query.column} FROM {query.table}"
+    print(sql, file=sys.stderr)
+
+    timeout = 20
+    limit = time.time() + timeout
+    conn = None
+    while not conn:
+        try:
+            conn = pymssql.connect(
+                server="mssql", user="SA", password="Your_password123!", database="test"
+            )
+        except OperationalError as e:
+            if time.time() >= limit:
+                raise Exception(
+                    f"Failed to connect to mssql after {timeout} seconds"
+                ) from e
+            time.sleep(1)
+
+    cursor = conn.cursor()
+    cursor.execute(sql)
+
     path = Path("/workspace/outputs/some_file.csv")
     path.parent.mkdir(parents=True, exist_ok=True)
-    path.touch()
+    with path.open(mode="w") as f:
+        f.write("patient_id,everything\n")
+
+        for patient_id, code in cursor:
+            f.write(f"{patient_id},{code}\n")
+
+    conn.close()
