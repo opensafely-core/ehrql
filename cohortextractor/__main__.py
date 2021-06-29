bb902138fe8fb4eadecdeec2298a651343ee0969
Becky Smith <becky.smith@thedatalab.org>
2021-06-29 10:29:14 +0100

    Move helper functions to query utils

--- cohortextractor/__main__.py
+++ cohortextractor/__main__.py
@@ -8,18 +8,7 @@ from pathlib import Path
 import sqlalchemy
 import sqlalchemy.exc
 
-
-def get_class_vars(cls):
-    default_vars = set(dir(type("ArbitraryEmptyClass", (), {})))
-    return [(key, value) for key, value in vars(cls).items() if key not in default_vars]
-
-
-def get_column_definitions(cohort_cls):
-    return {
-        key: value
-        for key, value in get_class_vars(cohort_cls)
-        if not key.startswith("_")
-    }
+from .query_utils import get_column_definitions
 
 
 def main():
