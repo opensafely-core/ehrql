2c2031abad6224d924bd44813a0ac81cbb6ade71
Becky Smith <becky.smith@thedatalab.org>
2021-07-02 12:01:13 +0100

    Move most of the query engine code into the MssqlQueryEngine for now

--- cohortextractor/query_engines/base.py
+++ cohortextractor/query_engines/base.py
@@ -1,12 +1,7 @@
-from collections import defaultdict
-
-from ..query_language import Column, QueryNode, Table, Value, ValueFromRow
-
-
 class BaseQueryEngine:
     """
-    A base QueryEngine to hold methods that deal with walking the AST are agnostic to how
-    the specific queries are built.  Inheriting classes must implement translating the groups
+    A base QueryEngine to hold methods that are agnostic to how the specific queries are built.
+    Inheriting classes must implement translating the AST and groups
     of linear node paths into their particular flavour of tables (SQL, pandas dataframes etc)
     """
 
@@ -19,54 +14,6 @@ class BaseQueryEngine:
         """
         self.column_definitions = column_definitions
         self.backend = backend
-        # Walk over all nodes in the query DAG looking for output nodes (leaf
-        # nodes which represent a value or a column of values) and group them
-        # together by "type" and "source" (source being the parent node from
-        # which they are derived). Each such group of outputs can be generated
-        # by a single query so we want them grouped together.
-        self.output_groups = defaultdict(list)
-        for node in self.walk_query_dag(column_definitions.values()):
-            if self.is_output_node(node):
-                self.output_groups[self.get_type_and_source(node)].append(node)
-
-    def walk_query_dag(self, nodes):
-        parents = []
-        for node in nodes:
-            yield node
-            for attr in ("source", "value"):
-                reference = getattr(node, attr, None)
-                if isinstance(reference, QueryNode):
-                    parents.append(reference)
-        if parents:
-            yield from self.walk_query_dag(parents)
-
-    @staticmethod
-    def is_output_node(node):
-        return isinstance(node, (Value, Column))
-
-    def get_type_and_source(self, node):
-        assert self.is_output_node(node)
-        return type(node), node.source
-
-    @staticmethod
-    def get_output_column_name(node):
-        # TODO deal with ValueFromAggregate
-        if isinstance(node, (ValueFromRow, Column)):
-            return node.column
-        else:
-            raise TypeError(f"Unhandled type: {node}")
-
-    @staticmethod
-    def get_node_list(node):
-        node_list = []
-        while True:
-            node_list.append(node)
-            if type(node) is Table:
-                break
-            else:
-                node = node.source
-        node_list.reverse()
-        return node_list
 
     def execute_query(self):
         """
