a2c2a39888b402cbcfc0b97d6eda08341d523fed
Becky Smith <becky.smith@thedatalab.org>
2021-07-05 11:02:26 +0100

    More configurable query_engine tests

--- tests/test_query_engine.py
+++ tests/test_query_engine.py
@@ -1,9 +1,10 @@
 import pytest
 import sqlalchemy
-from sql_setup import Base, Events, RegistrationHistory
+from sql_setup import Base, Events, PositiveTests, RegistrationHistory
 from sqlalchemy.orm import sessionmaker
 
-from cohortextractor.backends.mock import MockBackend
+from cohortextractor.backends import MockBackend
+from cohortextractor.backends.base import BaseBackend, Column, SQLTable
 from cohortextractor.main import extract
 from cohortextractor.query_engines.mssql import MssqlQueryEngine
 from cohortextractor.query_language import table
@@ -11,28 +12,81 @@ from cohortextractor.query_utils import get_column_definitions
 from tests.conftest import is_fast_mode
 
 
+DEFAULT_TABLES = {
+    "practice_registrations": SQLTable(
+        source="practice_registrations",
+        columns=dict(patient_id=Column("int", source="PatientId")),
+    ),
+    "clinical_events": SQLTable(
+        source="events",
+        columns=dict(
+            code=Column("varchar", source="EventCode"),
+            date=Column("varchar", source="Date"),
+        ),
+    ),
+}
+
+
 @pytest.fixture
-def db_engine_and_session():
-    def create_engine_and_setup_session(db_url, drivername="mssql+pymssql"):
+def mock_backend():
+    class MockTestBackend(BaseBackend):
+        """
+        A backend class which has no class-defined tables and sets up its tables
+        on init
+        """
+
+        backend_id = "testing"
+        query_engine_class = MssqlQueryEngine
+
+        def __init__(self, database_url, **tables):
+            super(MockTestBackend, self).__init__(database_url)
+            for table_name, sql_table in tables.items():
+                setattr(self, table_name, sql_table)
+            self.tables = tables.keys()
+
+    def create_backend(database_url, tables=None):
+        if tables is None:
+            tables = DEFAULT_TABLES
+        return MockTestBackend(database_url, **tables)
+
+    return create_backend
+
+
+@pytest.fixture
+def setup_test_database(database, load_data):
+    db_url = database.host_url()
+
+    def setup(input_data, drivername="mssql+pymssql"):
+        if not is_fast_mode():
+            # call Load data with a dummy SQL command; this will just ensure the container
+            # is started, with the test database created
+            load_data(sql="GO")
+
+        # Create engine
         url = sqlalchemy.engine.make_url(db_url)
         url = url.set(drivername=drivername)
         engine = sqlalchemy.create_engine(url, echo=True, future=True)
-
+        # Reset the schema
         Base.metadata.drop_all(engine)
         Base.metadata.create_all(engine)
+        # Create session
         Session = sessionmaker()
         Session.configure(bind=engine)
-        return engine, Session()
+        session = Session()
+        # Load test data
+        session.add_all(input_data)
+        session.commit()
 
-    return create_engine_and_setup_session
+    return setup
 
 
 def test_backend_tables():
     """Test that a backend registers its table names"""
+    # Use the base MockBackend for this test, so we're testing the real __init_subclass__ method
     assert MockBackend.tables == {"practice_registrations", "clinical_events"}
 
 
-def test_mssql_query_engine():
+def test_mssql_query_engine(mock_backend):
     """Test the simplest Cohort definition that just selects a single column"""
 
     class Cohort:
@@ -40,7 +94,7 @@ def test_mssql_query_engine():
 
     column_definitions = get_column_definitions(Cohort)
     query_engine = MssqlQueryEngine(
-        column_definitions=column_definitions, backend=MockBackend(database_url=None)
+        column_definitions=column_definitions, backend=mock_backend(database_url=None)
     )
 
     sql = query_engine.get_sql()
@@ -59,26 +113,17 @@ def test_mssql_query_engine():
     )
 
 
-def set_up_test_data(database, load_data, db_engine_and_session):
-    if not is_fast_mode():
-        # call Load data with a dummy SQL command; this will just ensure the container
-        # is started, with the test database created
-        load_data(sql="GO")
-    # Create the sqlalchemy engine and set up the session for the test data
-    engine, session = db_engine_and_session(db_url=database.host_url())
-    # Load test data
-    reg = RegistrationHistory(PatientId=1, StpId="STP1")
-    event1 = Events(PatientId=1, EventCode="Code1")
-    event2 = Events(PatientId=2, EventCode="Code2")
-    session.add_all([reg, event1, event2])
-    session.commit()
-
-
 @pytest.mark.integration
 def test_run_generated_sql_get_single_column_default_population(
-    database, load_data, db_engine_and_session
+    database, setup_test_database, mock_backend
 ):
-    set_up_test_data(database, load_data, db_engine_and_session)
+
+    input_data = [
+        RegistrationHistory(PatientId=1, StpId="STP1"),
+        Events(PatientId=1, EventCode="Code1"),
+        Events(PatientId=2, EventCode="Code2"),
+    ]
+    setup_test_database(input_data)
 
     # Cohort to extract all clinical events and return just the code column
     # Note that the RegistrationHistory table is used for the default population query
@@ -90,7 +135,7 @@ def test_run_generated_sql_get_single_column_default_population(
     column_definitions = get_column_definitions(Cohort)
 
     query_engine = MssqlQueryEngine(
-        column_definitions=column_definitions, backend=MockBackend(database.host_url())
+        column_definitions=column_definitions, backend=mock_backend(database.host_url())
     )
     with query_engine.execute_query() as result:
         assert list(result) == [(1, "Code1")]
@@ -98,9 +143,14 @@ def test_run_generated_sql_get_single_column_default_population(
 
 @pytest.mark.integration
 def test_run_generated_sql_get_single_column_specified_population(
-    database, load_data, db_engine_and_session
+    database, setup_test_database, mock_backend
 ):
-    set_up_test_data(database, load_data, db_engine_and_session)
+    input_data = [
+        RegistrationHistory(PatientId=1, StpId="STP1"),
+        Events(PatientId=1, EventCode="Code1"),
+        Events(PatientId=2, EventCode="Code2"),
+    ]
+    setup_test_database(input_data)
 
     # Cohort to extract all clinical events and return just the code column
     # Note that the RegistrationHistory table is used for the default population query
@@ -113,15 +163,61 @@ def test_run_generated_sql_get_single_column_specified_population(
     column_definitions = get_column_definitions(Cohort)
 
     query_engine = MssqlQueryEngine(
-        column_definitions=column_definitions, backend=MockBackend(database.host_url())
+        column_definitions=column_definitions, backend=mock_backend(database.host_url())
     )
     with query_engine.execute_query() as result:
         assert list(result) == [(1, "Code1")]
 
 
 @pytest.mark.integration
-def test_extract_get_single_column(database, load_data, db_engine_and_session):
-    set_up_test_data(database, load_data, db_engine_and_session)
+def test_run_generated_sql_get_multiple_columns(
+    database, setup_test_database, mock_backend
+):
+    input_data = [
+        RegistrationHistory(PatientId=1, StpId="STP1"),
+        RegistrationHistory(PatientId=2, StpId="STP1"),
+        Events(PatientId=1, EventCode="Code1"),
+        Events(PatientId=2, EventCode="Code2"),
+        PositiveTests(PatientId=1, PositiveResult=True),
+        PositiveTests(PatientId=2, PositiveResult=True),
+        PositiveTests(PatientId=2, PositiveResult=False),
+    ]
+    setup_test_database(input_data)
+
+    # Cohort to extract all clinical events and positive tests
+    class Cohort:
+        output_value = table("clinical_events").get("code")
+        positive = table("positive_tests").get("result")
+
+    column_definitions = get_column_definitions(Cohort)
+
+    backend_tables = {
+        **DEFAULT_TABLES,
+        "positive_tests": SQLTable(
+            source="pos_tests",
+            columns=dict(result=Column("bool", source="PositiveResult")),
+        ),
+    }
+    backend = mock_backend(database.host_url(), tables=backend_tables)
+    query_engine = MssqlQueryEngine(
+        column_definitions=column_definitions, backend=backend
+    )
+    with query_engine.execute_query() as result:
+        assert list(result) == [
+            (1, "Code1", True),
+            (2, "Code2", True),
+            (2, "Code2", False),
+        ]
+
+
+@pytest.mark.integration
+def test_extract_get_single_column(database, setup_test_database, mock_backend):
+    input_data = [
+        RegistrationHistory(PatientId=1, StpId="STP1"),
+        Events(PatientId=1, EventCode="Code1"),
+        Events(PatientId=2, EventCode="Code2"),
+    ]
+    setup_test_database(input_data)
 
     # Cohort to extract all clinical events and return just the code column
     # Note that the RegistrationHistory table is used for the default population query
@@ -130,18 +226,17 @@ def test_extract_get_single_column(database, load_data, db_engine_and_session):
     class Cohort:
         output_value = table("clinical_events").get("code")
 
-    backend = MockBackend(database.host_url())
-    result = extract(Cohort, backend)
+    result = extract(Cohort, mock_backend(database.host_url()))
     assert list(result) == [{"patient_id": 1, "output_value": "Code1"}]
 
 
-def test_invalid_table():
+def test_invalid_table(mock_backend):
     class Cohort:
         output_value = table("unknown").get("code")
 
     column_definitions = get_column_definitions(Cohort)
     query_engine = MssqlQueryEngine(
-        column_definitions=column_definitions, backend=MockBackend(database_url=None)
+        column_definitions=column_definitions, backend=mock_backend(database_url=None)
     )
     with pytest.raises(ValueError, match="Unknown table 'unknown'"):
         query_engine.get_sql()
