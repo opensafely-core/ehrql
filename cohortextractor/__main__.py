a8885c2dd7219b2d3b5f461a4211f3b2ec64634e
Becky Smith <becky.smith@thedatalab.org>
2021-11-05 17:40:17 +0000

    Make validate_cohort a separate command

--- cohortextractor/__main__.py
+++ cohortextractor/__main__.py
@@ -2,7 +2,7 @@ import os
 from argparse import ArgumentParser
 from pathlib import Path
 
-from .main import generate_cohort, generate_measures
+from .main import generate_cohort, generate_measures, run_cohort_action, validate_cohort
 
 
 def existing_python_file(value):
@@ -44,6 +44,28 @@ generate_cohort_parser.add_argument(
     help="Validate the cohort definition against the specified backend",
 )
 
+validate_cohort_parser = subparsers.add_parser(
+    "validate_cohort",
+    help="Validate the cohort definition against the specified backend",
+)
+validate_cohort_parser.set_defaults(which="validate_cohort")
+
+validate_cohort_parser.add_argument(
+    "backend",
+    type=str,
+    choices=["tpp", "graphnet"],
+)
+validate_cohort_parser.add_argument(
+    "--cohort-definition",
+    help="The path of the file where the cohort is defined",
+    type=existing_python_file,
+)
+validate_cohort_parser.add_argument(
+    "--output",
+    help="Path and filename (or pattern) of the file(s) where the output will be written",
+    type=Path,
+)
+
 generate_measures_parser = subparsers.add_parser(
     "generate_measures", help="Generate measures from cohort data"
 )
@@ -70,22 +92,26 @@ options = parser.parse_args()
 
 
 if options.which == "generate_cohort":
-    if (
-        not (options.dummy_data_file or os.environ.get("DATABASE_URL"))
-        and not options.validate_backend
-    ):
+    if not (options.dummy_data_file or os.environ.get("DATABASE_URL")):
         parser.error(
             "error: either --dummy-data-file or DATABASE_URL environment variable is required"
         )
 
-    generate_cohort(
+    run_cohort_action(
+        generate_cohort,
         definition_path=options.cohort_definition,
         output_file=options.output,
         db_url=os.environ.get("DATABASE_URL"),
         backend_id=os.environ.get("OPENSAFELY_BACKEND"),
         dummy_data_file=options.dummy_data_file,
         temporary_database=os.environ.get("TEMP_DATABASE_NAME"),
-        validate_backend=options.validate_backend,
+    )
+elif options.which == "validate_cohort":
+    run_cohort_action(
+        validate_cohort,
+        definition_path=options.cohort_definition,
+        output_file=options.output,
+        backend_id=options.backend,
     )
 elif options.which == "generate_measures":
     generate_measures(
--- cohortextractor/main.py
+++ cohortextractor/main.py
@@ -17,44 +17,24 @@ from .validate_dummy_data import validate_dummy_data
 log = structlog.getLogger()
 
 
-def generate_cohort(
-    definition_path,
-    output_file,
-    backend_id,
-    db_url,
-    dummy_data_file=None,
-    temporary_database=None,
-    validate_backend=None,
+def run_cohort_action(
+    cohort_action_function, definition_path, output_file, **function_kwargs
 ):
-    if validate_backend:
-        log.info(f"Validating cohort for {str(definition_path)}")
-    else:
-        log.info(
-            f"Generating cohort  as {output_file}",
-        )
-    log.debug(
-        "args:",
-        definition_path=definition_path,
-        output_file=output_file,
-        backend=backend_id,
-        dummy_data_file=dummy_data_file,
-    )
+    log.info(f"Running {cohort_action_function.__name__} for {str(definition_path)}")
+    log.debug("args:", **function_kwargs)
 
     output_file.parent.mkdir(parents=True, exist_ok=True)
     module = load_module(definition_path)
-
     cohort_class_generator, index_date_range = load_cohort_generator(module)
+    if len(index_date_range) > 1 and "*" not in output_file.name:
+        # ensure we have a replaceable pattern as an output file when multiple
+        # dates ranges are to be output
+        raise ValueError(f"No output pattern found in output file {output_file}")
 
     for index_date in index_date_range:
         if index_date is not None:
             log.info(f"Setting index_date to {index_date}")
             date_suffix = index_date
-            if len(index_date_range) > 1 and "*" not in output_file.name:
-                # ensure we have a replaceable pattern as an output file when multiple
-                # dates ranges are to be output
-                raise ValueError(
-                    f"No output pattern found in output file {output_file}"
-                )
         else:
             date_suffix = ""
 
@@ -63,31 +43,49 @@ def generate_cohort(
             if index_date
             else cohort_class_generator()
         )
-        output_file_with_date = _replace_filepath_pattern(output_file, date_suffix)
-        if validate_backend:
-            if index_date:
-                log.info("Validating for index date", index_date=index_date)
-            backend = BACKENDS[validate_backend](
-                db_url, temporary_database=temporary_database
-            )
-            results = validate(cohort, backend)
-            log.info("Validation succeeded")
-            write_validation_output(results, output_file_with_date)
+        cohort_action_function(
+            cohort, index_date, output_file, date_suffix, **function_kwargs
+        )
 
-        elif dummy_data_file and not db_url:
-            dummy_data_file_with_date = Path(
-                str(dummy_data_file).replace("*", date_suffix)
-            )
-            validate_dummy_data(
-                cohort, dummy_data_file_with_date, output_file_with_date
-            )
-            shutil.copyfile(dummy_data_file_with_date, output_file_with_date)
-        else:
-            backend = BACKENDS[backend_id](
-                db_url, temporary_database=temporary_database
-            )
-            results = extract(cohort, backend)
-            write_output(results, output_file_with_date)
+
+def generate_cohort(
+    cohort,
+    index_date,
+    output_file,
+    date_suffix,
+    backend_id,
+    db_url,
+    dummy_data_file=None,
+    temporary_database=None,
+):
+    output_file_with_date = _replace_filepath_pattern(output_file, date_suffix)
+    if index_date:
+        log.info("Generating cohort for index date", index_date=index_date)
+
+    if dummy_data_file and not db_url:
+        dummy_data_file_with_date = Path(str(dummy_data_file).replace("*", date_suffix))
+        validate_dummy_data(cohort, dummy_data_file_with_date, output_file_with_date)
+        shutil.copyfile(dummy_data_file_with_date, output_file_with_date)
+    else:
+        backend = BACKENDS[backend_id](db_url, temporary_database=temporary_database)
+        results = extract(cohort, backend)
+        write_output(results, output_file_with_date)
+
+
+def validate_cohort(
+    cohort,
+    index_date,
+    output_file,
+    date_suffix,
+    backend_id,
+):
+    output_file_with_date = _replace_filepath_pattern(output_file, date_suffix)
+    if index_date:
+        log.info("Validating for index date", index_date=index_date)
+    backend = BACKENDS[backend_id](database_url=None)
+    results = validate(cohort, backend)
+    log.info("Validation succeeded")
+    write_validation_output(results, output_file_with_date)
 
 
 def generate_measures(definition_path, input_file, output_file):
