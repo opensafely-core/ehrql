9713d7eca29fc8e930bfa0e93b1a39a6f426cbd8
Becky Smith <becky.smith@thedatalab.org>
2021-11-11 14:58:28 +0000

    Rename test_validate_backend to test_validate_cohort

--- tests/end_to_end/test_end_to_end_tpp.py
+++ tests/end_to_end/test_end_to_end_tpp.py
@@ -79,7 +79,7 @@ def test_validate_cohort(load_study, cohort_extractor_in_process_no_database):
     actual_results = cohort_extractor_in_process_no_database(
         study, backend="tpp", action=validate_cohort
     )
-    # validate_cohort exceeds and outputs SQL
+    # validate_cohort succeeds and outputs SQL
     with open(actual_results) as actual_file:
         actual_data = actual_file.readlines()
         assert actual_data[0].startswith("SELECT * INTO")
