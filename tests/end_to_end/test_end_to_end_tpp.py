5af483a03bc45722c62aeb2ae420f3a8100e69cf
Becky Smith <becky.smith@thedatalab.org>
2021-11-11 11:45:57 +0000

    Add a smoke test using dummy data

--- tests/end_to_end/test_end_to_end_tpp.py
+++ tests/end_to_end/test_end_to_end_tpp.py
@@ -65,6 +65,15 @@ def test_dummy_data(load_study, cohort_extractor_in_process_no_database):
     assert_results_equivalent(actual_results, study.expected_results())
 
 
+@pytest.mark.smoke
+def test_dummy_data_smoke_test(load_study, cohort_extractor_in_container):
+    study = load_study("end_to_end_tests_tpp", definition_file="tpp_cohort.py")
+    actual_results = cohort_extractor_in_container(
+        study, backend="tpp", use_dummy_data=True
+    )
+    assert_results_equivalent(actual_results, study.expected_results())
+
+
 def test_validate_cohort(load_study, cohort_extractor_in_process_no_database):
     study = load_study("end_to_end_tests_tpp", definition_file="tpp_cohort.py")
     actual_results = cohort_extractor_in_process_no_database(
