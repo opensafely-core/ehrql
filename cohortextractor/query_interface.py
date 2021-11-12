86616d71a85c77686021798454bf4506b3cc504e
Ben Butler-Cole <ben@bridesmere.com>
2021-11-12 10:12:56 +0000

    Don't construct query model until the DSL is compiled

--- cohortextractor/query_interface.py
+++ cohortextractor/query_interface.py
@@ -4,33 +4,40 @@ from cohortextractor import query_language
 
 class QueryBuilder:
     def __init__(self, table_name):
-        self.table = query_language.table(table_name)
+        self.table_name = table_name
+        self._filter = None
 
     def filter(self, *args, **kwargs):  # noqa: A003
-        self.table = self.table.filter(*args, **kwargs)
+        self._filter = (args, kwargs)
         return self
 
     def select_column(self, column_name):
-        return Column(self.table, column_name)
+        return Column(self.table_name, self._filter, column_name)
 
 
 class Column:
-    def __init__(self, table, column_name):
-        self.table = table
+    def __init__(self, table_name, filter, column_name):  # noqa: A002
+        self.table_name = table_name
+        self.filter = filter
         self.column_name = column_name
 
     def make_one_row_per_patient(self, reduce_function):
-        return Variable(self.table, self.column_name, reduce_function)
+        return Variable(self.table_name, self.filter, self.column_name, reduce_function)
 
 
 class Variable:
-    def __init__(self, table, column_name, reduce_function):
-        self.table = table
+    def __init__(self, table_name, filter, column_name, reduce_function):  # noqa: A002
+        self.table_name = table_name
+        self.filter = filter
         self.column_name = column_name
         self.reduce_function = reduce_function
 
     def compile_to_query_language(self):
         """Compile the query language Value"""
-        patient = self.reduce_function(self.table)
+        table = query_language.table(self.table_name)
+        if self.filter:
+            filter_args, filter_kwargs = self.filter
+            table = table.filter(*filter_args, **filter_kwargs)
+        patient = self.reduce_function(table)
         column = patient.get(self.column_name)
         return column
