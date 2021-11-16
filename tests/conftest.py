b031458eb33c95b44a95dde67380fba1eeeac1e7
David Evans <dave.evans@thedatalab.org>
2021-11-16 13:26:45 +0000

    Remove need to pass backend name to test setup function
    We only needed this to get the appropriate SQLAlchemy `MetaData()`
    instance in order to create the schema, but we already have this as the
    `metadata` attribute attached to all the ORM objects we're passing in.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,9 @@ from sqlalchemy.orm import sessionmaker
 
 from cohortextractor.definition.base import cohort_registry
 
-from .lib import mock_backend
 from .lib.databases import make_database, make_spark_database, wait_for_database
-from .lib.databricks_schema import Base as DatabricksBase
 from .lib.docker import Containers
-from .lib.graphnet_schema import Base as GraphnetBase
-from .lib.tpp_schema import Base as TppBase
 from .lib.util import iter_flatten
-
-
-BASES = {"tpp": TppBase, "graphnet": GraphnetBase, "databricks": DatabricksBase}
 
 
 @pytest.fixture(scope="session")
@@ -47,18 +40,21 @@ def database(request, containers, mssql_dir):
 
 @pytest.fixture
 def setup_test_database(database):
-    def setup(input_data, base=mock_backend.Base):
+    def setup(input_data):
+        input_data = list(iter_flatten(input_data))
         # Create engine
         engine = database.engine()
         # Reset the schema
-        base.metadata.drop_all(engine)
-        base.metadata.create_all(engine)
+        metadata = input_data[0].metadata
+        assert all(item.metadata is metadata for item in input_data)
+        metadata.drop_all(engine)
+        metadata.create_all(engine)
         # Create session
         Session = sessionmaker()
         Session.configure(bind=engine)
         session = Session()
         # Load test data
-        session.bulk_save_objects(iter_flatten(input_data))
+        session.bulk_save_objects(input_data)
         session.commit()
 
     return setup
@@ -66,8 +62,8 @@ def setup_test_database(database):
 
 @pytest.fixture
 def setup_backend_database(setup_test_database):
-    def setup(*data, backend="tpp"):
-        setup_test_database(data, base=BASES[backend])
+    def setup(*data, backend=None):
+        setup_test_database(data)
 
     yield setup
 
@@ -82,18 +78,20 @@ def spark_database(containers):
 @pytest.fixture
 def setup_spark_database(spark_database):
     def setup(*input_data, backend=None):
-        base = BASES[backend]
+        input_data = list(iter_flatten(input_data))
         # Create engine
         engine = spark_database.engine()
         # Reset the schema
-        base.metadata.drop_all(engine)
-        base.metadata.create_all(engine)
+        metadata = input_data[0].metadata
+        assert all(item.metadata is metadata for item in input_data)
+        metadata.drop_all(engine)
+        metadata.create_all(engine)
         # Create session
         Session = sessionmaker()
         Session.configure(bind=engine)
         session = Session()
         # Load test data
-        session.bulk_save_objects(iter_flatten(input_data))
+        session.bulk_save_objects(input_data)
         session.commit()
 
     return setup
