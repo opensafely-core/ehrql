54af80301235ad94df79d0107daa5951ec936aef
Becky Smith <becky.smith@thedatalab.org>
2021-11-18 15:39:51 +0000

    Cohort fixtures/helper class with a default population variable

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import docker
 import docker.errors
 import pytest
 
+from cohortextractor.concepts import tables
+from cohortextractor.definition import Cohort, exists
 from cohortextractor.definition.base import cohort_registry
 
 from .lib.databases import make_database, make_spark_database, wait_for_database
@@ -36,3 +38,14 @@ def spark_database(containers):
 def cleanup_register():
     yield
     cohort_registry.reset()
+
+
+@pytest.fixture
+def cohort_with_population():
+    cohort = Cohort()
+    registrations_table = tables.registrations
+    registered = registrations_table.select_column(
+        registrations_table.patient_id
+    ).make_one_row_per_patient(exists)
+    cohort.set_population(registered)
+    yield cohort
