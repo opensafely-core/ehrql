c812c8040a3f2c3665c1fa959f21b6761b9c5406
David Evans <dave.evans@thedatalab.org>
2021-11-18 14:55:39 +0000

    Remove test dependency on MockBackend

--- tests/backends/test_base.py
+++ tests/backends/test_base.py
@@ -1,11 +1,31 @@
-from ..lib.mock_backend import MockBackend
+from cohortextractor.backends.base import BaseBackend, Column, MappedTable
+from cohortextractor.query_engines.base_sql import BaseSQLQueryEngine
 
 
 def test_backend_tables():
     """Test that a backend registers its table names"""
-    assert MockBackend.tables == {
-        "practice_registrations",
-        "clinical_events",
+
+    class BasicBackend(BaseBackend):
+        backend_id = "basic_test_backend"
+        query_engine_class = BaseSQLQueryEngine
+        patient_join_column = "patient_id"
+
+        patients = MappedTable(
+            source="Patient",
+            columns=dict(
+                date_of_birth=Column("date", source="DateOfBirth"),
+            ),
+        )
+
+        clinical_events = MappedTable(
+            source="coded_events",
+            columns=dict(
+                code=Column("code", source="EventCode"),
+                date=Column("date", source="Date"),
+            ),
+        )
+
+    assert BasicBackend.tables == {
         "patients",
-        "positive_tests",
+        "clinical_events",
     }
