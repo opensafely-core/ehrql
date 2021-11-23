79660273f58023bd146b5f8a83d6042fa34e804e
Becky Smith <becky.smith@thedatalab.org>
2021-11-23 14:49:35 +0000

    feat: Simple column type-checking for backend contracts

--- cohortextractor/concepts/table_contract.py
+++ cohortextractor/concepts/table_contract.py
@@ -33,3 +33,15 @@ class TableContract:
                 f" contract for '{cls.__name__}'\n\n"
                 f"Missing columns: {', '.join(missing_columns)}"
             )
+        backend_table = getattr(backend, table_name)
+        for column in cls.columns:
+            backend_column_type = backend_table.columns[column].type
+            contract_column_type = cls.columns[column].type
+
+            if backend_column_type not in contract_column_type.allowed_backend_types:
+                raise BackendContractError(
+                    f"\n'{backend.__name__}.{table_name}' does not correctly implement the"
+                    f" contract for '{cls.__name__}'\n\n"
+                    f"Column {column} is defined with an invalid type.\n\n"
+                    f"Allowed types are: {', '.join( contract_column_type.allowed_backend_types)}"
+                )
--- cohortextractor/concepts/types.py
+++ cohortextractor/concepts/types.py
@@ -1,15 +1,31 @@
 class BaseType:
-    pass
+    """
+    Base class for Column types defined in a Contract
+    """
+
+    # Subclasses must specify the backend Column types that they allow, as
+    # tuples of one or more keys from cohortextractor.sqlalchemy_types.TYPES_BY_NAME
+    allowed_backend_types: tuple = NotImplemented
 
 
 class PseudoPatientId(BaseType):
-    pass
+    """A type representing a pseudonymised patient ID"""
+
+    allowed_backend_types = ("integer", "varchar")
 
 
 class Date(BaseType):
-    pass
+    """A type representing a date"""
+
+    allowed_backend_types = ("date",)
 
 
 class Choice(BaseType):
+    """
+    A type which restricts column values to a limited set of choices.
+    """
+
+    allowed_backend_types = ("integer", "varchar")
+
     def __init__(self, *choices):
         self.choices = choices
