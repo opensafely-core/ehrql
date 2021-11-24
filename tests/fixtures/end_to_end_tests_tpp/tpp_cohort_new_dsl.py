27f3f4e59e8e27acd551797b41d0e5418fdd715b
Peter Inglesby <peter.inglesby@gmail.com>
2021-11-24 12:23:08 +0000

    Don't instantiate ClinicalEvents in test

--- tests/fixtures/end_to_end_tests_tpp/tpp_cohort_new_dsl.py
+++ tests/fixtures/end_to_end_tests_tpp/tpp_cohort_new_dsl.py
@@ -4,7 +4,7 @@ from cohortextractor.dsl import Cohort
 
 cohort = Cohort()
 cohort.set_population(tables.registrations.exists_for_patient())
-events = tables.ClinicalEvents()
+events = tables.clinical_events
 cohort.date = events.sort_by(events.date).first_for_patient().select_column(events.date)
 cohort.event = (
     events.sort_by(events.code).first_for_patient().select_column(events.code)
