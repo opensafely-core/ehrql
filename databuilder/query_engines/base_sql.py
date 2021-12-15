4f63c03f79908a985f659342b07a6532d28e11f7
David Evans <dave.evans@thedatalab.org>
2021-12-15 18:04:45 +0000

    Used `cached_property` decorator to simplify code

--- databuilder/query_engines/base_sql.py
+++ databuilder/query_engines/base_sql.py
@@ -44,6 +44,7 @@ import copy
 import dataclasses
 import typing
 from collections import defaultdict
+from functools import cached_property
 from typing import Any, Optional, Union
 
 import sqlalchemy
@@ -492,19 +493,18 @@ class BaseSQLQueryEngine(BaseQueryEngine):
     #
     # DATABASE CONNECTION
     #
-    @property
+    @cached_property
     def engine(self):
-        if self._engine is None:
-            engine_url = sqlalchemy.engine.make_url(self.backend.database_url)
-            # Hardcode the specific SQLAlchemy dialect we want to use: this is the
-            # dialect the query engine will have been written for and tested with and we
-            # don't want to allow global config changes to alter this
-            engine_url._get_entrypoint = lambda: self.sqlalchemy_dialect
-            self._engine = sqlalchemy.create_engine(engine_url, future=True)
-            # The above relies on abusing SQLAlchemy internals so it's possible it will
-            # break in future -- we want to know immediately if it does
-            assert isinstance(self._engine.dialect, self.sqlalchemy_dialect)
-        return self._engine
+        engine_url = sqlalchemy.engine.make_url(self.backend.database_url)
+        # Hardcode the specific SQLAlchemy dialect we want to use: this is the
+        # dialect the query engine will have been written for and tested with and we
+        # don't want to allow global config changes to alter this
+        engine_url._get_entrypoint = lambda: self.sqlalchemy_dialect
+        engine = sqlalchemy.create_engine(engine_url, future=True)
+        # The above relies on abusing SQLAlchemy internals so it's possible it will
+        # break in future -- we want to know immediately if it does
+        assert isinstance(engine.dialect, self.sqlalchemy_dialect)
+        return engine
 
     def get_temp_table_name(self, name_hint):
         """
