34b3a5ce2e3433ceb55d51c7be0a48309a9c7036
David Evans <dave.evans@thedatalab.org>
2021-12-17 13:29:18 +0000

    Add basic test for CASE statement with string value

--- tests/query_engines/test_dialects.py
+++ tests/query_engines/test_dialects.py
@@ -47,3 +47,20 @@ def test_datetime_column_returns_date_if_typed_as_such(engine):
         datetime.datetime(2021, 4, 15, 10, 11, 12),
         datetime.date(2020, 10, 20),
     )
+
+
+def test_case_statement(engine):
+    """
+    Test a basic CASE statement returning a string value. This exposed a bug in the
+    string handling of our Spark dialect so it's useful to keep it around.
+    """
+    case_statement = sqlalchemy.case(
+        [
+            (sqlalchemy.literal(1) == 0, "foo"),
+            (sqlalchemy.literal(1) == 1, "bar"),
+        ]
+    )
+    query = sqlalchemy.select(case_statement.label("output"))
+    with engine.sqlalchemy_engine().connect() as conn:
+        results = list(conn.execute(query))
+    assert results[0]["output"] == "bar"
