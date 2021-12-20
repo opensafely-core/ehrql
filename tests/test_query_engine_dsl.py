edc7a586da1bcf32d2d4db1f4b62bbefc49f4fcc
Becky Smith <becky.smith@thedatalab.org>
2021-12-20 10:54:26 +0000

    Test comparator order

--- tests/test_query_engine_dsl.py
+++ tests/test_query_engine_dsl.py
@@ -28,3 +28,26 @@ def test_categorise_simple_comparisons(engine, cohort_with_population):
         dict(patient_id=2, height_group="tall"),
         dict(patient_id=3, height_group="missing"),
     ]
+
+
+def test_comparator_order(engine, cohort_with_population):
+    """Test that comparison operators work on both sides of the comparator"""
+    input_data = [patient(1, height=180), patient(2, height=200.5), patient(3)]
+    engine.setup(input_data)
+
+    patients = MockPatients()
+    height = patients.select_column(patients.height)
+    height_categories = {
+        "tall": 190 < height,
+        "short": 190 >= height,
+    }
+    height_group = categorise(height_categories, default="missing")
+    cohort = cohort_with_population
+    cohort.height_group = height_group
+
+    result = engine.extract(cohort)
+    assert result == [
+        dict(patient_id=1, height_group="short"),
+        dict(patient_id=2, height_group="tall"),
+        dict(patient_id=3, height_group="missing"),
+    ]
