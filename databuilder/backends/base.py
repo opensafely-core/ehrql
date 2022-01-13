718fd5988af0f24fe5a2ec7b3eadf4269295278b
Peter Inglesby <peter.inglesby@gmail.com>
2022-01-13 09:19:12 +0000

    Remove unnecessary method

--- databuilder/backends/base.py
+++ databuilder/backends/base.py
@@ -48,26 +48,15 @@ class BaseBackend:
         table.learn_patient_join(cls.patient_join_column)
 
     @classmethod
-    def validate_contract(cls, name, table):
-        """
-        Validates that the table correctly implements the contract it
-        claims to, if any
-        Args:
-            name: Name of table
-            table: SQLTable Subclass
-        """
-        contract = table.implements
-        if contract:
-            contract.validate_implementation(cls, name, table)
-
-    @classmethod
     def validate_all_contracts(cls) -> None:
         """
         Loops through all the tables in a backend and validates that
         each one meets any contract that it claims to implement
         """
         for name, table in cls.tables.items():
-            cls.validate_contract(name, table)
+            contract = table.implements
+            if contract:
+                contract.validate_implementation(cls, name, table)
 
     def get_table_expression(self, table_name):
         """
