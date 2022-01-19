5716441d4c216298b90c864f97c3aaa60416123f
George Hickman <george@ghickman.co.uk>
2022-01-19 10:59:45 +0000

    Test assert_temporary_tables_writable handling of DBAPIErrors

--- tests/query_engines/test_mssql_lib.py
+++ tests/query_engines/test_mssql_lib.py
@@ -5,6 +5,7 @@ import sqlalchemy
 
 from databuilder.query_engines.mssql_lib import (
     ReconnectableConnection,
+    assert_temporary_tables_writable,
     fetch_results_in_batches,
 )
 
@@ -228,3 +229,12 @@ def test_reconnectableconnection_reconnect_with_no_connection():
     with ReconnectableConnection(None) as conn:
         conn.reconnect()
         conn.reconnect()
+
+
+def test_assert_temporary_tables_writable_exception_wrapper():
+    conn = mock.MagicMock()
+    conn.execute.side_effect = sqlalchemy.exc.DBAPIError(None, None, None)
+
+    match = "^Unable to write temporary table with prefix 'unknown_table'$"
+    with pytest.raises(RuntimeError, match=match):
+        assert_temporary_tables_writable(conn, "unknown_table")
