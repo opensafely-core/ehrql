998ac2593b30ba3c4fb2ef75c4919968901733f4
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2022-02-07 12:47:54 +0000

    wip

--- databuilder/query_language.py
+++ databuilder/query_language.py
@@ -11,6 +11,7 @@ This contains just enough of erhQL to be able to extract the following dataset:
 from __future__ import annotations
 
 from . import query_model as qm
+from .query_model import SelectColumn, SelectPatientTable
 
 
 class Dataset:
@@ -22,22 +23,55 @@ class Dataset:
         super().__setattr__(name, series)
 
     def compile(self) -> dict[str, qm.Node]:  # noqa A003
-        return ...
+        variables = {}
+        for name, definition in self._variables():
+            variables[name] = definition.compile()
+        return {}
+
+    def _variables(self):
+        for name in dir(self):
+            attr = getattr(self, name)
+            if isinstance(attr, Series) or isinstance(attr, Column):
+                yield name, attr
 
 
-class PatientTable:
+class TableMeta(type):
+    def __setattr__(cls, name, value):
+        if isinstance(value, Column):
+            value.table = cls
+        return super().__setattr__(name, value)
+
+
+class PatientTable(metaclass=TableMeta):
     def __init__(self, name: str):
         self.name = name
+
+    def compile(self):  # noqa A003
+        return SelectPatientTable(name=self.name)
 
 
 class Column:
-    def __init__(self, name: str):
+    def __init__(self, name: str, series_type):
         self.name = name
+        self.series_type = series_type
+
+    def compile(self):  # noqa A003
+        return self.series_type(
+            SelectColumn(name=self.name, source=self.table.compile())
+        ).compile()
+
+
+class DateColumn(Column):
+    def __init__(self, name):
+        super().__init__(name, DateSeries)
 
 
 class Series:
     def __init__(self, qm_node: qm.Node):
         self.qm_node = qm_node
+
+    def compile(self):  # noqa A003
+        return self.qm_node
 
 
 class IdSeries(Series):
@@ -57,3 +91,6 @@ class DateSeries(Series):
     @property
     def year(self) -> IntSeries:
         return IntSeries(qm_node=...)
+
+    def __le__(self, other):
+        return BoolSeries()
--- tests/unit/test_query_language.py
+++ tests/unit/test_query_language.py
@@ -1,29 +1,58 @@
-from databuilder.query_language import (
-    Column,
-    Dataset,
-    DateSeries,
-    IdSeries,
-    PatientTable,
-)
+import datetime
 
+from databuilder.dsl import IdColumn
+from databuilder.query_language import Column, Dataset, DateColumn, PatientTable
 
 # This table definition is here as a convenience.
 # TODO: instantiate tables with a contract, and import tables from tests.lib.tables
+from databuilder.query_model import Function, SelectColumn, SelectPatientTable, Value
+
+
 class Patients(PatientTable):
-    patient_id = IdSeries(Column("patient_id"))
-    date_of_birth = DateSeries(Column("date_of_birth"))
+    def __init__(self):
+        super().__init__("patients")
+
+        for name in dir(self):
+            if not hasattr(Patients, name):
+                continue
+            attr = getattr(Patients, name)
+            if isinstance(attr, Column):
+                attr.table = self
+
+    patient_id = IdColumn("patient_id")
+    date_of_birth = DateColumn("date_of_birth")
 
 
-patients = Patients("patients")
+patients = Patients()
 
 
 def test_simple_dataset() -> None:
-    year_of_birth = patients.date_of_birth.year
+    year_of_birth = patients.date_of_birth
     dataset = Dataset()
-    dataset.set_population(year_of_birth <= 2000)  # TODO: why does this not typecheck?
+    dataset.set_population(
+        year_of_birth <= datetime.date(2000, 1, 1)
+    )  # TODO: why does this not typecheck?
     dataset.year_of_birth = year_of_birth
 
     assert dataset.compile() == {
-        "year_of_birth": ...,
+        "year_of_birth": SelectColumn(
+            name="date_of_birth", source=SelectPatientTable("patients")
+        ),
         "population": ...,
     }
+
+
+def test_get_column_from_patient_table():
+    # SELECT date_of_birth FROM patients
+    assert patients.date_of_birth.compile() == SelectColumn(
+        name="date_of_birth", source=SelectPatientTable("patients")
+    )
+
+
+def test_date_comparison():
+    assert (
+        patients.date_of_birth <= datetime.date(2000, 1, 1)
+    ).compile() == Function.LE(
+        lhs=SelectColumn(name="date_of_birth", source=SelectPatientTable("patients")),
+        rhs=Value(datetime.date(2000, 1, 1)),
+    )
