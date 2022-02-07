2ffaf5890b4bc3a0b5337c9df2959c4315f226b2
David Evans <dave.evans@thedatalab.org>
2022-02-07 09:50:47 +0000

    Refactor test queries into fixture

--- tests/test_query_model.py
+++ tests/test_query_model.py
@@ -1,8 +1,9 @@
+from types import SimpleNamespace
+
 import pytest
 
 from databuilder.query_model import (
     AggregateByPatient,
-    Code,
     DomainMismatchError,
     Filter,
     Function,
@@ -13,18 +14,27 @@ from databuilder.query_model import (
     SelectColumn,
     SelectTable,
     Sort,
+    Value,
 )
 
 
-def test_construct_basic_query():
+@pytest.fixture
+def queries():
+    q = SimpleNamespace()
     events = SelectTable("events")
     code = SelectColumn(events, "code")
     date = SelectColumn(events, "date")
-    vaccinations = Filter(events, Function.EQ(code, Code("abc123", system="ctv3")))
-    has_vaccination = AggregateByPatient.Exists(vaccinations)
-    first_vaccination = PickOneRowPerPatient(Sort(vaccinations, date), Position.FIRST)
-    assert isinstance(has_vaccination, OneRowPerPatientSeries)
-    assert isinstance(first_vaccination, OneRowPerPatientFrame)
+    vaccinations = Filter(events, Function.EQ(code, Value("abc123")))
+    q.has_vaccination = AggregateByPatient.Exists(
+        SelectColumn(vaccinations, "patient_id")
+    )
+    q.first_vaccination = PickOneRowPerPatient(Sort(vaccinations, date), Position.FIRST)
+    return q
+
+
+def test_queries_have_expected_types(queries):
+    assert isinstance(queries.has_vaccination, OneRowPerPatientSeries)
+    assert isinstance(queries.first_vaccination, OneRowPerPatientFrame)
 
 
 def test_mixing_domains_throws_error():
@@ -32,4 +42,4 @@ def test_mixing_domains_throws_error():
     vaccinations = SelectTable("vaccinations")
     vaccine_code = SelectColumn(vaccinations, "code")
     with pytest.raises(DomainMismatchError):
-        Filter(events, Function.EQ(vaccine_code, Code("abc123", system="ctv3")))
+        Filter(events, Function.EQ(vaccine_code, Value("abc123")))
