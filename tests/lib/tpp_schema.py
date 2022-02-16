71f6bd3794c4f68b28048225b8ce212a5b260690
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2022-02-16 15:50:52 +0000

    Remove some now-unused test infrastructure

--- tests/lib/tpp_schema.py
+++ tests/lib/tpp_schema.py
@@ -1,7 +1,7 @@
 import random
 
 import sqlalchemy.orm
-from sqlalchemy import Column, Date, DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy import Column, Date, ForeignKey, Integer, String
 
 Base = sqlalchemy.orm.declarative_base()
 rand = random.Random(12345)
@@ -31,36 +31,6 @@ def patient(patient_id=None, sex=None, dob=None, date_of_death=None, related=Non
     ]
 
 
-class Organisation(Base):
-    __tablename__ = "Organisation"
-    Organisation_ID = Column(Integer, primary_key=True)
-    Region = Column(String())
-
-
-class RegistrationHistory(Base):
-    __tablename__ = "RegistrationHistory"
-    Registration_ID = Column(Integer, primary_key=True)
-    Patient_ID = Column(Integer, ForeignKey("Patient.Patient_ID"))
-    Organisation_ID = Column(Integer, ForeignKey("Organisation.Organisation_ID"))
-    StartDate = Column(DateTime)
-    EndDate = Column(DateTime)
-
-
-def registration(start_date, end_date, organisation_id=None):
-    return RegistrationHistory(
-        StartDate=start_date, EndDate=end_date, Organisation_ID=organisation_id
-    )
-
-
-class CTV3Events(Base):
-    __tablename__ = "CodedEvent"
-    CodedEvent_ID = Column(Integer, primary_key=True)
-    Patient_ID = Column(Integer, ForeignKey("Patient.Patient_ID"))
-    CTV3Code = Column(String(collation="Latin1_General_BIN"))
-    ConsultationDate = Column(DateTime)
-    NumericValue = Column(Float)
-
-
 class APCS(Base):
     __tablename__ = "APCS"
     APCS_Ident = Column(Integer, primary_key=True)
@@ -74,13 +44,3 @@ def apcs(admission_date=None, codes=None):
         Admission_Date=(admission_date or "2012-12-12"),
         Der_Diagnosis_All=codes or "xyz",
     )
-
-
-class PatientAddress(Base):
-    __tablename__ = "PatientAddress"
-    PatientAddress_ID = Column(Integer, primary_key=True)
-    Patient_ID = Column(Integer, ForeignKey("Patient.Patient_ID"))
-    StartDate = Column(DateTime)
-    EndDate = Column(DateTime)
-    ImdRankRounded = Column(Integer)
-    MSOACode = Column(String)
