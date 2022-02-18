8e051e03fceac7ada730a0b5c8ca6a1be0ba4917
George Hickman <george@ghickman.co.uk>
2022-02-18 14:07:42 +0000

    Add a description attribute to each Constraint class

--- databuilder/contracts/constraints.py
+++ databuilder/contracts/constraints.py
@@ -16,6 +16,10 @@ class ChoiceConstraint(BaseConstraint):
     def __init__(self, *choices):
         self.choices = choices
 
+    @property
+    def description(self):
+        return ", ".join(self.choices)
+
     def validate(self, backend_table, column):
         """Validate against data"""
         # TODO
@@ -25,6 +29,8 @@ class NotNullConstraint(BaseConstraint):
     """
     Defines a constraint on a Column to ensure no null values
     """
+
+    description = "Must have a value"
 
     def validate(self, backend_table, column):
         """Validate values exist for all rows in column"""
@@ -36,6 +42,8 @@ class UniqueConstraint(BaseConstraint):
     Defines a constraint on a Column to ensure values are unique
     """
 
+    description = "Must be unique"
+
     def validate(self, backend_table, column):
         """Validate no duplicated values"""
         # TODO
@@ -45,6 +53,8 @@ class FirstOfMonthConstraint(BaseConstraint):
     """
     Defines a constraint on a date Column to ensure the day is always the first of the month
     """
+
+    description = "Must be the first day of a month"
 
     def validate(self, backend_table, column):
         """Validate against data"""
