4624a3bb841e7d728a3f904e1c08e441ab42986e
George Hickman <george@ghickman.co.uk>
2022-02-18 14:08:35 +0000

    when a docstring is None, return a list

--- databuilder/docs.py
+++ databuilder/docs.py
@@ -41,9 +41,12 @@ def _build_contracts():
 
 def _reformat_docstring(d):
     """Reformat docstring to make it easier to use in a markdown/HTML document."""
+    if d is None:
+        return []
+
     docstring = d.strip()
 
-    return [line.strip() for line in docstring.split("\n")]
+    return [line.strip() for line in docstring.splitlines()]
 
 
 def generate_docs(location=None):
