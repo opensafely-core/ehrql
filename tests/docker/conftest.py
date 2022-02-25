d4c3de86b1d2c328a861f8ac7829bc5547f98776
George Hickman <george@ghickman.co.uk>
2022-02-25 11:22:16 +0000

    test generate_dataset in docker with a dataset defintion

--- tests/docker/conftest.py
+++ tests/docker/conftest.py
@@ -7,11 +7,16 @@ import pytest
 def run_in_container(tmpdir, containers):
     workspace = Path(tmpdir.mkdir("workspace"))
 
-    def run(command, study=None):
-        return containers.run_fg(
+    def run(command):
+        output = containers.run_fg(
             image="databuilder:latest",
             command=command,
             volumes={workspace: {"bind": "/workspace", "mode": "rw"}},
         )
+
+        return {
+            "container_response": output,
+            "workspace_dir": workspace,
+        }
 
     return run
