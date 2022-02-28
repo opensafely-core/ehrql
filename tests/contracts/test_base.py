b16a3fefae4acdeebc0a0807a2398196239fb2fb
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2022-02-28 10:32:02 +0000

    Remove incomplete implementation of data validation

--- tests/contracts/test_base.py
+++ tests/contracts/test_base.py
@@ -5,10 +5,7 @@ from databuilder.contracts import types
 from databuilder.contracts.base import BackendContractError
 from databuilder.contracts.base import Column as ColumnContract
 from databuilder.contracts.base import TableContract
-from databuilder.contracts.constraints import ChoiceConstraint
 from databuilder.query_engines.base_sql import BaseSQLQueryEngine
-
-from ..lib.mock_backend import patient
 
 
 class PatientsContract(TableContract):
@@ -95,42 +92,3 @@ def test_validate_implementation_failure_invalid_type():
         match="Column date_of_birth is defined with an invalid type 'integer'.\n\nAllowed types are: date",
     ):
         PatientsContract.validate_implementation(BadBackend, "patients")
-
-
-def test_basic_validation_for_patients_contract_column_constraints(engine):
-    engine.setup(
-        patient(
-            1,
-            dob="1990-01-01",
-        ),
-        patient(2, dob="1991-02-01"),
-        patient(3, dob="1992-03-01", sex="X"),
-    )
-
-    # Basic table contract
-    class PatientsContract(TableContract):
-        patient_id = ColumnContract(type=types.PseudoPatientId())
-        date_of_birth = ColumnContract(type=types.Date())
-        sex = ColumnContract(
-            type=types.Choice("F", "M"),
-            constraints=[ChoiceConstraint()],
-        )
-
-    # Happy path is all there is for now
-    class GoodBackend(BaseBackend):
-        backend_id = "good_test_backend"
-        query_engine_class = BaseSQLQueryEngine
-        patient_join_column = "patient_id"
-
-        patients = MappedTable(
-            implements=PatientsContract,
-            source="Patient",
-            columns=dict(
-                date_of_birth=Column("date", source="DateOfBirth"),
-                sex=Column("varchar", source="Sex"),
-            ),
-        )
-
-    backend = GoodBackend(database_url=engine.database.host_url())
-    comment = "Note: This test is expected to fail when TableContract.validate_data is implemented"
-    assert PatientsContract.validate_data(backend, "patients") is None, comment
