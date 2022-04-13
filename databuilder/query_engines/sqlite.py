6f89411dc0d657b2b3ac8741e5d4470c17ac5d56
David Evans <dave.evans@thedatalab.org>
2022-04-13 16:19:31 +0100

    more hacking

--- databuilder/query_engines/sqlite.py
+++ databuilder/query_engines/sqlite.py
@@ -39,18 +39,18 @@ class SQLiteQueryEngine(BaseQueryEngine):
         return self.get_results_query(population_expression, variable_expressions)
 
     def get_results_query(self, population_expression, variable_expressions):
-        patient_table = self.get_patient_table(
+        patient_id_expr = self.get_patient_id_expr(
             population_expression, *variable_expressions.values()
         )
         columns = [
-            patient_table.c.patient_id.label("patient_id"),
+            patient_id_expr.label("patient_id"),
             *[expr.label(name) for name, expr in variable_expressions.items()],
         ]
         query = sqlalchemy.select(columns).where(population_expression)
-        query = add_implicit_joins(query)
+        query = apply_implicit_joins(query)
         return query
 
-    def get_patient_table(self, *expressions):
+    def get_patient_id_expr(self, *expressions):
         # TODO: This logic is still under discussion but this covers us for now: it
         # returns a Common Table Expression contain all patient_ids contained in all
         # tables references in the dataset definition
@@ -59,7 +59,7 @@ class SQLiteQueryEngine(BaseQueryEngine):
         id_selects = [
             sqlalchemy.select(table.c.patient_id).distinct() for table in tables
         ]
-        return sqlalchemy.union(*id_selects).cte()
+        return sqlalchemy.union(*id_selects).cte().c.patient_id
 
     @singledispatchmethod
     def get_sql(self, node):
@@ -135,76 +135,92 @@ class SQLiteQueryEngine(BaseQueryEngine):
     def get_sql_select_table(self, node):
         return self.backend.get_table_expression(node.name)
 
-    @get_sql.register(AggregateByPatient.Exists)
-    def get_sql_exists(self, node):
-        return self.aggregate_by_patient_non_nullable(
-            node.source, sqlalchemy.literal(True), False
-        )
-
-    @get_sql.register(AggregateByPatient.Count)
-    def get_sql_count(self, node):
-        return self.aggregate_by_patient_non_nullable(
-            node.source, sqlalchemy.func.count("*"), 0
-        )
-
-    def aggregate_by_patient(self, source, aggregation_func):
-        expression = self.get_sql(source)
-        if has_many_rows_per_patient(source):
-            query = self.get_select_query_for_node(source)
-        else:
-            query = sqlalchemy.select([self.get_patient_table(expression).c.patient_id])
-        query = query.add_columns(aggregation_func(expression))
-        query = query.group_by(query.selected_columns[0])
-        query = add_implicit_joins(query)
-        aggregated_table = query.cte()
-        return aggregated_table.c[1]
-
-    def aggregate_by_patient_non_nullable(self, source, aggregation_func, empty_value):
-        value = self.aggregate_by_patient(source, lambda _: aggregation_func)
-        return sqlalchemy.func.coalesce(value, empty_value)
+    # We handle `Filter` and `Sort` operations separately inside the
+    # `get_select_query_for_node` method; in this context we just pass through the
+    # source expression as if the operation wasn't there. This awkwardness is due to the
+    # mismatch between SQL and QueryModel semantics. In SQL, applying a filter or a sort
+    # to a table doesn't change the way you reference columns from that table. Instead,
+    # these are handled by WHERE/ORDER BY clauses elsewhere. So likewise, here we pass
+    # table references unchanged through Filter/Sort and handle them elsewhere.
+    @get_sql.register(Sort)
+    @get_sql.register(Filter)
+    def get_sql_sort_and_filter(self, node):
+        return self.get_sql(node.source)
 
     @get_sql.register(AggregateByPatient.Sum)
     def get_sql_sum(self, node):
         return self.aggregate_by_patient(node.source, sqlalchemy.func.sum)
 
-    @get_sql.register(Sort)
-    def get_sql_sort(self, node):
-        return self.get_sql(node.source)
+    @get_sql.register(AggregateByPatient.Exists)
+    def get_sql_exists(self, node):
+        return self.aggregate_by_patient_non_nullable(
+            node.source, sqlalchemy.literal(True), empty_value=False
+        )
+
+    @get_sql.register(AggregateByPatient.Count)
+    def get_sql_count(self, node):
+        return self.aggregate_by_patient_non_nullable(
+            node.source, sqlalchemy.func.count("*"), empty_value=0
+        )
 
     @get_sql.register(PickOneRowPerPatient)
     def get_sql_pick_one_row_per_patient(self, node):
         query = self.get_select_query_for_node(node.source)
 
+        # TODO: Really we only want to select the columns from the base table which
+        # we're actually going to use. In the old world we did some pre-processing of
+        # the graph to make this information available at the point we need it. But for
+        # now we just select everything for the base table.
         base_table = query.get_final_froms()[0]
         already_selected = set(query.selected_columns)
         other_columns = [c for c in base_table.c.values() if c not in already_selected]
+        query = query.add_columns(*other_columns)
 
-        sort_columns = query.partition_order_clauses
-        if node.position == Position.LAST:
-            sort_columns = [column.desc() for column in sort_columns]
-
-        # Number rows sequentially over the order by columns for each patient id
-        row_num = (
-            sqlalchemy.func.row_number()
-            .over(partition_by=query.selected_columns[0], order_by=sort_columns)
-            .label("_row_num")
+        # Add an extra "row number" column to the query which gives the position of each
+        # row within its patient_id group as implied by the order clauses
+        desc = node.position == Position.LAST
+        order_clauses = [c.desc() if desc else c for c in query.partition_order_clauses]
+        query = query.add_columns(
+            sqlalchemy.func.row_number().over(
+                partition_by=query.selected_columns[0], order_by=order_clauses
+            )
         )
-        query = query.add_columns(*other_columns, row_num)
-        query = add_implicit_joins(query)
+
+        query = apply_implicit_joins(query)
+
+        # Make the above into a subquery and pull out the relevant columns
         subquery = query.alias()
         subquery_columns = list(subquery.columns)
+        output_columns = subquery_columns[:-1]
+        row_number = subquery_columns[-1]
 
-        partitioned_query = sqlalchemy.select(subquery_columns[:-1]).where(
-            subquery_columns[-1] == 1
-        )
+        # Select the first row for each patient according to the above row numbering
+        partitioned_query = sqlalchemy.select(output_columns).where(row_number == 1)
+
         return partitioned_query.cte()
+
+    def aggregate_by_patient(self, node, aggregation_func):
+        expression = self.get_sql(node)
+        if has_many_rows_per_patient(node):
+            query = self.get_select_query_for_node(node)
+        else:
+            query = sqlalchemy.select([self.get_patient_id_expr(expression)])
+        query = query.add_columns(aggregation_func(expression))
+        query = query.group_by(query.selected_columns[0])
+        query = apply_implicit_joins(query)
+        aggregated_table = query.cte()
+        return aggregated_table.c[1]
+
+    def aggregate_by_patient_non_nullable(self, node, aggregation_func, empty_value):
+        value = self.aggregate_by_patient(node, lambda _: aggregation_func)
+        return sqlalchemy.func.coalesce(value, empty_value)
 
     def get_select_query_for_node(self, node):
         frame = get_many_rows_per_patient_frame(node)
         root_frame, filters, sorts = get_frame_operations(frame)
         table = self.get_sql(root_frame)
-        where_clauses = [self.get_sql(f) for f in filters]
-        order_clauses = [self.get_sql(s) for s in sorts]
+        where_clauses = [self.get_sql(f.condition) for f in filters]
+        order_clauses = [self.get_sql(s.sort_by) for s in sorts]
 
         query = sqlalchemy.select([table.c.patient_id])
         if where_clauses:
@@ -213,10 +229,6 @@ class SQLiteQueryEngine(BaseQueryEngine):
         query.partition_order_clauses = tuple(order_clauses)
 
         return query
-
-    @get_sql.register(Filter)
-    def get_sql_filter(self, node):
-        return self.get_sql(node.source)
 
     @contextlib.contextmanager
     def execute_query(self):
@@ -238,70 +250,77 @@ class SQLiteQueryEngine(BaseQueryEngine):
         return engine
 
 
-def get_referenced_tables(clause):
-    """
-    Given an arbitrary SQLAlchemy clause determine what tables it references
-    """
-    if isinstance(clause, sqlalchemy.Table):
-        return (clause,)
-    if hasattr(clause, "table") and clause.table is not None:
-        return (clause.table,)
-    else:
-        tables = ()
-        for child in clause.get_children():
-            tables += get_referenced_tables(child)
-        return tables
-
-
 def get_unique_tables(*expressions):
     tables = {}
     for expression in expressions:
         if isinstance(expression, sqlalchemy.sql.ClauseElement):
-            for table in get_referenced_tables(expression):
+            for table in expression._from_objects:
                 tables[table] = True
     return list(tables.keys())
 
 
-def get_many_rows_per_patient_frame(*nodes):
-    children = []
-    for node in nodes:
-        if isinstance(node, ManyRowsPerPatientFrame):
-            return node
-        else:
-            for child in get_input_nodes(node):
-                if not isinstance(
-                    child, (OneRowPerPatientSeries, OneRowPerPatientFrame)
-                ):
-                    children.append(child)
-    # Given the constraints on the query model this can only happen if we're passed a
-    # one-row-per-patient node, which we should never be
-    assert children, "No ManyRowsPerPatientFrame found"
-    return get_many_rows_per_patient_frame(*children)
+def apply_implicit_joins(query):
+    """
+    Find any table references in `query` which aren't part of an explicit JOIN and make
+    them part of an explicit LEFT OUTER JOIN using the `patient_id` column (or whatever
+    the first column in the SELECT is called — we assume this is always the primary join
+    column)
+    """
+    # We structure our queries so that the column to be joined on is always the first
+    join_key = query.selected_columns[0]
+    join_column = join_key.name
+    # Any tables referenced by clauses in the query which aren't yet explicity joined on
+    # will be returned as extra FROM clauses by the `get_final_froms()` method
+    implicit_joins = query.get_final_froms()[1:]
+    for table in implicit_joins:
+        query = query.join(table, table.c[join_column] == join_key, isouter=True)
+    return query
+
+
+def get_many_rows_per_patient_frame(node):
+    frames = list(get_many_rows_per_patient_frames(node))
+    # By the QueryModel domain constraints, any node of many-rows dimension should have
+    # exactly one many-rows frame from which it's derived
+    assert len(frames) == 1, f"Expected exactly one frame, got: {frames}"
+    return frames[0]
+
+
+def get_many_rows_per_patient_frames(node):
+    """
+    Return all ManyRowsPerPatientFrames directly referenced by the suppplied QueryNode
+    """
+    # Once we've hit a ManyRowsPerPatientFrame we don't dig down further into its
+    # children; this is what we mean by "directly reference"
+    if isinstance(node, ManyRowsPerPatientFrame):
+        return {node}
+    # Similarly, we don't dig down through one-row-per-patient nodes
+    elif isinstance(node, (OneRowPerPatientSeries, OneRowPerPatientFrame)):
+        return set()
+    else:
+        return set().union(
+            *[
+                get_many_rows_per_patient_frames(child)
+                for child in get_input_nodes(node)
+            ]
+        )
 
 
 def get_frame_operations(frame):
+    """
+    Given a ManyRowsPerPatientFrame, destructure it into a base SelectTable operation,
+    plus separate lists of Filter and Sort operations
+    """
     filters = []
     sorts = []
     while True:
         type_ = type(frame)
         if type_ is Filter:
-            filters.insert(0, frame.condition)
+            filters.insert(0, frame)
             frame = frame.source
         elif type_ is Sort:
-            sorts.insert(0, frame.sort_by)
+            sorts.insert(0, frame)
             frame = frame.source
         elif type_ is SelectTable:
             return frame, filters, sorts
         else:
-            assert False, f"Unhandled type: {frame}"
-
-
-def add_implicit_joins(query):
-    join_key = query.selected_columns[0]
-    join_column = join_key.name
-    joined_tables = {query.get_final_froms()[0]}
-    for table in get_referenced_tables(query):
-        if table not in joined_tables:
-            joined_tables.add(table)
-            query = query.join(table, table.c[join_column] == join_key, isouter=True)
-    return query
+            assert False, f"Unexpected type: {frame}"
