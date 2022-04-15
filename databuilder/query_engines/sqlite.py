67128a5546d4dc06c99e8a5489832e8c8890e5c8
David Evans <dave.evans@thedatalab.org>
2022-04-15 16:49:49 +0100

    Support PickOneRowPerPatient operation
    I think the key move here (which took me a long while to hit on) is to
    handle Filter/Sort operations — or Frame->Frame operations more
    generally — separately from Series operations. So we now ignore Filter
    and Sort operations until the point where we come to construct a SELECT
    query.

--- databuilder/query_engines/sqlite.py
+++ databuilder/query_engines/sqlite.py
@@ -5,7 +5,17 @@ import sqlalchemy
 from sqlalchemy.dialects.sqlite.pysqlite import SQLiteDialect_pysqlite
 from sqlalchemy.sql import operators
 
-from databuilder.query_model import Function, SelectColumn, SelectPatientTable, Value
+from databuilder.query_model import (
+    Filter,
+    Function,
+    PickOneRowPerPatient,
+    Position,
+    SelectColumn,
+    SelectPatientTable,
+    SelectTable,
+    Sort,
+    Value,
+)
 from databuilder.sqlalchemy_utils import get_referenced_tables
 
 from .base import BaseQueryEngine
@@ -116,13 +126,93 @@ class SQLiteQueryEngine(BaseQueryEngine):
         source = self.get_sql(node.source)
         return source.c[node.name]
 
-    # We have to apply caching here otherwise we generate disinct objects representing
+    # We have to apply caching here otherwise we generate distinct objects representing
     # the same table and this confuses SQLAlchemy into generating queries with ambiguous
     # table references
+    @get_sql.register(SelectTable)
     @get_sql.register(SelectPatientTable)
     @cache
     def get_sql_select_table(self, node):
         return self.backend.get_table_expression(node.name)
+
+    # We ignore Filter and Sort operations completely at this point in the code and just
+    # pass the underlying table reference through. It's only later, when building the
+    # SELECT query for a given Frame, that we make use of these. This is in order to
+    # mirror the semantics of SQL whereby columns are selected directly from the
+    # underlying table and filters and sorts are handled separately using WHERE/ORDER BY
+    # clauses.
+    @get_sql.register(Sort)
+    @get_sql.register(Filter)
+    def get_sql_sort_and_filter(self, node):
+        return self.get_sql(node.source)
+
+    @get_sql.register(PickOneRowPerPatient)
+    def get_sql_pick_one_row_per_patient(self, node):
+        query = self.get_select_query_from_frame(node.source)
+
+        # TODO: Really we only want to select the columns from the base table which
+        # we're actually going to use. In the old world we did some pre-processing of
+        # the graph to make this information available at the point we need it and we
+        # should probably reinstate that. But for now we just select all columns from
+        # the base table.
+        query = select_all_columns_from_base_table(query)
+
+        # Add an extra "row number" column to the query which gives the position of each
+        # row within its patient_id partition as implied by the order clauses
+        order_clauses = self.get_order_clauses_from_frame(node.source)
+        if node.position == Position.LAST:
+            order_clauses = [c.desc() for c in order_clauses]
+        query = query.add_columns(
+            sqlalchemy.func.row_number().over(
+                partition_by=query.selected_columns[0], order_by=order_clauses
+            )
+        )
+
+        query = apply_patient_joins(query)
+
+        # Make the above into a subquery and pull out the relevant columns. Note, we're
+        # deliberately using a subquery rather than `reify_query()` here as we want the
+        # database to have the chance to spot that we're just fetching the first row
+        # from each partition and optimise the query.
+        subquery = query.alias()
+        subquery_columns = list(subquery.columns)
+        output_columns = subquery_columns[:-1]
+        row_number = subquery_columns[-1]
+
+        # Select the first row for each patient according to the above row numbering
+        partitioned_query = sqlalchemy.select(output_columns).where(row_number == 1)
+
+        return self.reify_query(partitioned_query)
+
+    def reify_query(self, query):
+        """
+        By "reify" we just mean turning a SELECT query into something that can function
+        as a table in other SQLAlchemy constructs. There are various ways to do this
+        e.g. using `.alias()` to make a sub-query, using `.cte()` to make a Common Table
+        Expression, or writing the results of the query to a temporary table.
+        """
+        return query.cte()
+
+    def get_select_query_from_frame(self, frame):
+        """
+        Given a ManyRowsPerPatientFrame return the corresponding SELECT query with the
+        appropriate filter operations applied
+        """
+        root_frame, filters, _ = get_frame_operations(frame)
+        table = self.get_sql(root_frame)
+        where_clauses = [self.get_sql(f.condition) for f in filters]
+        query = sqlalchemy.select([table.c.patient_id])
+        if where_clauses:
+            query = query.where(sqlalchemy.and_(*where_clauses))
+        return query
+
+    def get_order_clauses_from_frame(self, frame):
+        """
+        Given a ManyRowsPerPatientFrame return the order_by clauses created by any Sort
+        operations which have been applied
+        """
+        _, _, sorts = get_frame_operations(frame)
+        return [self.get_sql(s.sort_by) for s in sorts]
 
     @contextlib.contextmanager
     def execute_query(self):
@@ -175,3 +265,33 @@ def apply_patient_joins(query):
     for table in implicit_joins:
         query = query.join(table, table.c[join_column] == join_key, isouter=True)
     return query
+
+
+# TODO: This is hopefully a temporary workaround. See the comment at this function's one
+# call site for more detail.
+def select_all_columns_from_base_table(query):
+    base_table = query.get_final_froms()[0]
+    already_selected = {c.name for c in query.selected_columns}
+    other_columns = [c for c in base_table.c.values() if c.name not in already_selected]
+    return query.add_columns(*other_columns)
+
+
+def get_frame_operations(frame):
+    """
+    Given a ManyRowsPerPatientFrame, destructure it into a base SelectTable operation,
+    plus separate lists of Filter and Sort operations
+    """
+    filters = []
+    sorts = []
+    while True:
+        type_ = type(frame)
+        if type_ is Filter:
+            filters.insert(0, frame)
+            frame = frame.source
+        elif type_ is Sort:
+            sorts.insert(0, frame)
+            frame = frame.source
+        elif type_ is SelectTable:
+            return frame, filters, sorts
+        else:
+            assert False, f"Unexpected type: {frame}"
