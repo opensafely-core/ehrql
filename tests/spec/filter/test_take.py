8ed0a11ea18eff65c4832380edfc342e3e9148bc
David Evans <dave.evans@thedatalab.org>
2022-05-10 11:23:29 +0100

    Add test to expose bug in in-memory database

--- tests/spec/filter/test_take.py
+++ tests/spec/filter/test_take.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ..tables import e
 
 title = "Including rows"
@@ -52,5 +54,53 @@ def test_take_with_expr(spec_test):
             1: (101 + 102 + 103),
             2: 201,
             3: None,
+        },
+    )
+
+
+@pytest.mark.xfail_in_memory
+def test_chain_multiple_takes(spec_test):
+    table_data = {
+        e: """
+              | i1 | b1
+            --+----+----
+            1 | 1  | T
+            1 | 2  | T
+            1 | 3  | F
+            """,
+    }
+
+    spec_test(
+        table_data,
+        e.take(e.i1 >= 2).take(e.b1).i1.sum_for_patient(),
+        {
+            1: 2,
+        },
+    )
+
+
+# TODO: This isn't actually a spec test and should be moved elsewhere once we have the
+# mechanics in place to run non-spec tests against multiple query engines. But I want to
+# capture somewhere the currently working behaviour of the in-memory engine and this
+# will do for now.
+def test_multiple_takes_without_chaining(spec_test):
+    table_data = {
+        e: """
+              | i1 | b1
+            --+----+----
+            1 | 1  | T
+            1 | 2  | T
+            1 | 3  | F
+            """,
+    }
+
+    f1 = e.take(e.i1 >= 2)
+    f2 = f1.take(f1.b1)
+
+    spec_test(
+        table_data,
+        f2.i1.sum_for_patient(),
+        {
+            1: 2,
         },
     )
