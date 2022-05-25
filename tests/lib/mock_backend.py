7f85e021251e1527dd0dc833675d9037918648f9
David Evans <dave.evans@thedatalab.org>
2022-05-25 16:08:25 +0100

    Move some ORM utility functions into `tests.lib.util`

--- tests/lib/mock_backend.py
+++ tests/lib/mock_backend.py
@@ -4,6 +4,7 @@ from databuilder.backends.base import BaseBackend, Column, MappedTable, QueryTab
 from databuilder.query_engines.legacy_mssql import MssqlQueryEngine
 
 from . import contracts
+from .util import next_id, null
 
 
 def backend_factory(query_engine_cls):
@@ -103,19 +104,6 @@ def backend_factory(query_engine_cls):
 
 # Create a default MockBackend instance for code which is still expecting this
 MockBackend = backend_factory(MssqlQueryEngine)
-
-# Generate an integer sequence to use as default IDs. Normally you'd rely on the DBMS to
-# provide these, but we need to support DBMSs like Spark which don't have this feature.
-next_id = iter(range(1, 2**63)).__next__
-
-
-# We need each NULL-able column to have an explicit default of NULL. Without this,
-# SQLAlchemy will just omit empty columns from the INSERT. That's fine for most DBMSs
-# but Spark needs every column in the table to be specified, even if it just has a NULL
-# value. Note: we have to use a callable returning `None` here because if we use `None`
-# directly SQLAlchemy interprets this is "there is no default".
-def null():
-    return None
 
 
 Base = sqlalchemy.orm.declarative_base()
--- tests/lib/util.py
+++ tests/lib/util.py
@@ -20,3 +20,21 @@ def iter_flatten(iterable, iter_classes=(list, tuple)):
             yield from iter_flatten(item, iter_classes)
         else:
             yield item
+
+
+# SQLAlchemy ORM Utils
+#
+
+
+# Generate an integer sequence to use as default IDs. Normally you'd rely on the DBMS to
+# provide these, but we need to support DBMSs like Spark which don't have this feature.
+next_id = iter(range(1, 2**63)).__next__
+
+
+# We need each NULL-able column to have an explicit default of NULL. Without this,
+# SQLAlchemy will just omit empty columns from the INSERT. That's fine for most DBMSs
+# but Spark needs every column in the table to be specified, even if it just has a NULL
+# value. Note: we have to use a callable returning `None` here because if we use `None`
+# directly SQLAlchemy interprets this is "there is no default".
+def null():
+    return None
