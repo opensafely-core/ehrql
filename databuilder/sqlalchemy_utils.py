af5db642ab8c763aaffc75d6089328912a9da104
David Evans <dave.evans@thedatalab.org>
2022-06-15 16:13:28 +0100

    Add GeneratedTable class
    This provides a generic mechanism for constructing "multi-step" queries
    i.e. queries that require creating intermediate objects before fetching
    their results. These intermediate objects could be temporary tables,
    persistent tables, views, or anything else so long as they function
    syntactically as a table.
    
    The core bit of graph walking logic was ported directly from the
    `legacy_sqlalchemy_utils` module. It's a bit clunky and imperative, and
    no doubt there's a nicer recursive solution here, but it will do for
    now.

--- databuilder/sqlalchemy_utils.py
+++ databuilder/sqlalchemy_utils.py
@@ -1,4 +1,6 @@
+import sqlalchemy
 from sqlalchemy.sql.elements import AsBoolean, BinaryExpression, BooleanClauseList
+from sqlalchemy.sql.visitors import iterate
 
 
 def is_predicate(clause):
@@ -26,3 +28,88 @@ def is_predicate(clause):
     if isinstance(clause, BinaryExpression):
         return clause._is_implicitly_boolean
     return False
+
+
+class GeneratedTable(sqlalchemy.Table):
+    """
+    This wraps a standard SQLAlchemy Table and adds a pair of extra attributes
+    which are a sequence of queries needed to create and populate this table and a
+    sequence (possibly empty) of queries needed to clean it up.
+
+    This provides a generic mechanism for constructing "multi-step" queries i.e. queries
+    that require creating intermediate objects before fetching their results. These
+    intermediate objects could be temporary tables, persistent tables, views, or
+    anything else so long as it functions syntactically as a table.
+    """
+
+    setup_queries = ()
+    cleanup_queries = ()
+
+
+def get_setup_and_cleanup_queries(query):
+    """
+    Given a SQLAlchemy query find all GeneratedTables embeded in it and return a pair:
+
+        setup_queries, cleanup_queries
+
+    which are the combination of all the setup and cleanup queries from those
+    GeneratedTables in the correct order for execution.
+    """
+    # GeneratedTables can be arbitrarily nested in that their setup queries can
+    # themselves contain references to GeneratedTables and so on. So we need to
+    # recursively unpack these, taking note of a number of subtleties:
+    #
+    #  1. We need to keep track of what level of nesting each table is at. The setup
+    #     queries for the most deeply nested tables must be run first so they exist by
+    #     the time queries for their dependant tables get run.
+    #
+    #  2. The same table can occur at multiple levels of nesting so we need to track the
+    #     highest level for each table.
+    #
+    #  3. By construction there can be no cycles in this graph *except* that each
+    #     table's setup queries will contain a reference to itself (as part of a "create
+    #     this table" statement). This means we need to keep track of which tables we've
+    #     seen on each given branch to avoid looping.
+    #
+    queries = [(query, 0, set())]
+    table_levels = {}
+    while queries:
+        next_query, level, seen = queries.pop(0)
+        for table in get_generated_tables(next_query):
+            if table in seen:
+                continue
+            # We may end up setting this multiple times, but if so we'll set the highest
+            # level last, which is what we want
+            table_levels[table] = level
+            # Add all of the table's setup and cleanup queries to the stack to be checked
+            queries.extend(
+                (query, level + 1, seen | {table})
+                for query in [*table.setup_queries, *table.cleanup_queries]
+            )
+
+    # Sort tables in reverse order by level
+    tables = sorted(
+        table_levels.keys(),
+        key=table_levels.__getitem__,
+        reverse=True,
+    )
+
+    setup_queries = flatten_iter(t.setup_queries for t in tables)
+    # Concatenate cleanup queries into one list, but in reverse order to that which we
+    # created them in. This means that if there are any database-level dependencies
+    # between the tables (e.g. if one is a materialized view over another) then we don't
+    # risk errors by trying to delete objects which still have dependents.
+    cleanup_queries = flatten_iter(t.cleanup_queries for t in reversed(tables))
+
+    return setup_queries, cleanup_queries
+
+
+def get_generated_tables(clause):
+    """
+    Return any GeneratedTable objects referenced by a SQLAlchemy ClauseElement
+    """
+    return [elem for elem in iterate(clause) if isinstance(elem, GeneratedTable)]
+
+
+def flatten_iter(nested_iters):
+    return [i for sub_iter in nested_iters for i in sub_iter]
