0b4b35ed6648ea7197e21201a38b773097137ec9
David Evans <dave.evans@thedatalab.org>
2022-06-16 15:09:03 +0100

    Discard temporary fixture
    Now that Spark and MSSQL are part of the default `engine` fixture we
    don't need this.

--- tests/docker/test_drivers.py
+++ tests/docker/test_drivers.py
@@ -1,24 +1,11 @@
-from types import SimpleNamespace
-
 import pytest
 
 
-# TODO: Until we move the mssql and spark engines back from "legacy" status the default
-# engine fixture doesn't give us the coverage we need so we replace it here. Once these
-# are included in the default engine fixture we can delete this one. But we'll also need
-# to add a check the test to skip on the in-memory and SQLite engines because the test
-# won't apply to them.
-@pytest.fixture(params=["mssql", "spark"])
-def engine(request, mssql_database, spark_database):
-    if request.param == "mssql":
-        return SimpleNamespace(name=request.param, database=mssql_database)
-    elif request.param == "spark":
-        return SimpleNamespace(name=request.param, database=spark_database)
-    else:
-        assert False
-
-
 def test_driver_in_container(run_in_container, engine):
+    # This test doesn't make sense for these in-memory databases
+    if engine.name in {"in_memory", "sqlite"}:
+        pytest.skip()
+
     backends = {
         "mssql": "tpp",
         "spark": "databricks",
