affec40e101060928e2f1a8e078ad16bda8bf948
David Evans <dave.evans@thedatalab.org>
2022-06-17 12:47:29 +0100

    Use temporary VIEWs in place of CTEs for Spark
    Given that these aren't materialized I don't know if there's any actual
    advantage over CTEs here:
    https://spark.apache.org/docs/3.0.0-preview/sql-ref-syntax-ddl-create-view.html
    
    However this is what our original NHSD proof-of-concept did so I'd
    rather keep things consistent with that. It's also easier to switch to
    an alternative mechanism once we've put the basic structures in place so
    I think it's worthwhile.

--- databuilder/query_engines/spark.py
+++ databuilder/query_engines/spark.py
@@ -2,7 +2,8 @@ import sqlalchemy
 
 from databuilder import sqlalchemy_types
 from databuilder.query_engines.base_sql import BaseSQLQueryEngine
-from databuilder.query_engines.spark_dialect import SparkDialect
+from databuilder.query_engines.spark_dialect import CreateTemporaryViewAs, SparkDialect
+from databuilder.sqlalchemy_utils import GeneratedTable
 
 
 class SparkQueryEngine(BaseSQLQueryEngine):
@@ -18,3 +19,14 @@ class SparkQueryEngine(BaseSQLQueryEngine):
         new_date = sqlalchemy.func.date_add(date, num_days)
         # Tell SQLAlchemy that the result is a date without doing any CASTing in the SQL
         return sqlalchemy.type_coerce(new_date, sqlalchemy_types.Date())
+
+    def reify_query(self, query):
+        # Define a table object with the same columns as the query
+        table_name = self.next_intermediate_table_name()
+        columns = [
+            sqlalchemy.Column(c.name, c.type, key=c.key) for c in query.selected_columns
+        ]
+        table = GeneratedTable(table_name, sqlalchemy.MetaData(), *columns)
+        # Create a temporary (session-scoped) view from the query
+        table.setup_queries = [CreateTemporaryViewAs(table, query)]
+        return table
