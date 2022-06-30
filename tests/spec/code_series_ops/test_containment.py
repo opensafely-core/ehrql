116ea14938759a49913eb3273925aef65c68e1db
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-06-30 14:37:25 +0100

    test: remove unecessary test
    as this is already covered in test_is_not_in

--- tests/spec/code_series_ops/test_containment.py
+++ tests/spec/code_series_ops/test_containment.py
@@ -64,27 +64,3 @@ def test_is_in_codelist_csv(spec_test):
             4: None,
         },
     )
-
-
-def test_is_not_in_codelist_csv(spec_test):
-
-    codelist = codelist_from_csv_lines(
-        [
-            "code",
-            "abc",
-            "ghi",
-        ],
-        column="code",
-        system="snomedct",
-    )
-
-    spec_test(
-        table_data,
-        p.c1.is_not_in(codelist),
-        {
-            1: False,
-            2: True,
-            3: False,
-            4: None,
-        },
-    )
