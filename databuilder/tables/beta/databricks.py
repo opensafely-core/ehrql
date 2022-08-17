268fcd9c435365c02242a4721951473f8977f6b3
David Evans <dave.evans@thedatalab.org>
2022-08-17 17:26:50 +0100

    Add `databuilder.tables.beta` modules for each backend
    We use the `beta` namespace because we want to allow people to start
    using Data Builder without commiting to the these as the final schemas.

---
+++ databuilder/tables/beta/databricks.py
@@ -0,0 +1,3 @@
+from databuilder.contracts.wip import hospital_admissions, patients, prescriptions
+
+__all__ = ["hospital_admissions", "prescriptions", "patients"]
---
+++ databuilder/tables/beta/tpp.py
@@ -0,0 +1,17 @@
+from databuilder.contracts.universal import patients
+from databuilder.contracts.wip import (
+    clinical_events,
+    covid_test_results,
+    hospitalizations,
+    patient_address,
+    practice_registrations,
+)
+
+__all__ = [
+    "patients",
+    "clinical_events",
+    "covid_test_results",
+    "hospitalizations",
+    "patient_address",
+    "practice_registrations",
+]
