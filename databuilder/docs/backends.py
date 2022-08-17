d772877663cdf7e38931ad2614cee0f6480d1976
David Evans <dave.evans@thedatalab.org>
2022-08-17 16:09:57 +0100

    Temporarily remove contracts from backend docs

--- databuilder/docs/backends.py
+++ databuilder/docs/backends.py
@@ -2,7 +2,6 @@ import operator
 
 from ..backends.base import BaseBackend
 from ..module_utils import get_sibling_subclasses
-from .common import build_hierarchy
 
 
 def build_backends():
@@ -10,14 +9,9 @@ def build_backends():
     backends.sort(key=operator.attrgetter("__name__"))
 
     for backend in backends:
-        # get the full name for all implemented contracts the backend implements
-        tables = [getattr(backend, name) for name in backend.tables]
-        contract_classes = [table.implements for table in tables if table.implements]
-        contract_names = [
-            "/".join([*build_hierarchy(c), c.__name__]) for c in contract_classes
-        ]
-
         yield {
             "name": backend.__name__,
-            "contracts": contract_names,
+            # TODO: Re-establish a listing of the tables a given backend supports once
+            # we decide exactly how this is going to work
+            "contracts": [],
         }
