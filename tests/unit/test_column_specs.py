d2be070fe4f5488afc25e500306147f177954380
David Evans <dave.evans@thedatalab.org>
2022-09-13 15:10:32 +0100

    Use "primitive" types in ColumnSpec

--- tests/unit/test_column_specs.py
+++ tests/unit/test_column_specs.py
@@ -1,5 +1,6 @@
 import datetime
 
+from databuilder.codes import SNOMEDCTCode
 from databuilder.column_specs import ColumnSpec, get_column_specs
 from databuilder.query_model import (
     AggregateByPatient,
@@ -11,14 +12,17 @@ from databuilder.query_model import (
 
 def test_get_column_specs():
     patients = SelectPatientTable(
-        "patients", schema=TableSchema({"date_of_birth": datetime.date})
+        "patients",
+        schema=TableSchema({"date_of_birth": datetime.date, "code": SNOMEDCTCode}),
     )
     variables = dict(
         population=AggregateByPatient.Exists(patients),
         dob=SelectColumn(patients, "date_of_birth"),
+        code=SelectColumn(patients, "code"),
     )
     column_specs = get_column_specs(variables)
     assert column_specs == {
         "patient_id": ColumnSpec(type=int, nullable=False),
         "dob": ColumnSpec(type=datetime.date, nullable=True),
+        "code": ColumnSpec(type=str, nullable=True),
     }
