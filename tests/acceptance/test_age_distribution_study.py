e9bfc6a14c5dc30694f483cd4f09cd53666cb0ef
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2022-09-15 15:44:12 +0100

    Smoke test now actually calculates ages

--- tests/acceptance/test_age_distribution_study.py
+++ tests/acceptance/test_age_distribution_study.py
@@ -6,16 +6,18 @@ from tests.lib.tpp_schema import patient
 def test_generate_dataset(study, mssql_database):
     mssql_database.setup(
         patient(dob=datetime(1910, 5, 5)),
+        patient(dob=datetime(1970, 5, 5)),
+        patient(dob=datetime(1990, 5, 5)),
         patient(dob=datetime(2010, 5, 5)),
     )
 
     study.setup_from_repo(
         "opensafely/test-age-distribution",
-        "before-change",
+        "main",
         "analysis/dataset_definition.py",
     )
     study.generate(mssql_database, "databuilder.backends.tpp.TPPBackend")
     results = study.results()
 
-    assert len(results) == 1
-    assert results[0]["year_of_birth"] == "2010"
+    assert len(results) == 2
+    assert results[0]["age"] == "32"
