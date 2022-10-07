cbfb80722679cf1a698d065835f7c4a19094aeac
David Evans <dave.evans@thedatalab.org>
2022-10-07 11:01:43 +0100

    feat: Switch CSV engine to use in-memory database

--- databuilder/query_engines/csv.py
+++ databuilder/query_engines/csv.py
@@ -1,19 +1,17 @@
 from pathlib import Path
 
-from sqlalchemy.orm import Session
-
 from databuilder.orm_utils import (
     orm_classes_from_qm_tables,
     read_orm_models_from_csv_directory,
 )
-from databuilder.query_engines.sqlite import SQLiteQueryEngine
+from databuilder.query_engines.in_memory import InMemoryQueryEngine
+from databuilder.query_engines.in_memory_database import InMemoryDatabase
 from databuilder.query_model import get_table_nodes
 
 
-class CSVQueryEngine(SQLiteQueryEngine):
+class CSVQueryEngine(InMemoryQueryEngine):
     """
-    This is a subclass of the SQLite engine which loads its data from a supplied
-    directory of CSV files. It exists purely for demonstration purposes.
+    Subclass of the in-memory engine which loads its data from a directory of CSV files
     """
 
     def __init__(self, *args, **kwargs):
@@ -21,23 +19,18 @@ class CSVQueryEngine(SQLiteQueryEngine):
         # Treat the DSN as the path to a directory of CSVs
         self.csv_directory = self.dsn
         # This gives us an in memory SQLite database
-        self.dsn = "sqlite://"
+        self.dsn = InMemoryDatabase()
 
     def get_results(self, variable_definitions):
-        # Given the variables supplied determine the tables used, create corresponding
-        # ORM classes and use them to initialise the database schema
+        # Given the variables supplied determine the tables used and create
+        # corresponding ORM classes
         table_nodes = get_table_nodes(*variable_definitions.values())
         orm_classes = orm_classes_from_qm_tables(table_nodes)
-        assert orm_classes
-        orm_classes[0].metadata.create_all(self.engine)
-
         # Populate the database using CSV files in the supplied directory
         input_data = read_orm_models_from_csv_directory(
             Path(self.csv_directory), orm_classes
         )
-        with Session(self.engine) as session:
-            session.bulk_save_objects(input_data)
-            session.commit()
+        self.dsn.setup(input_data)
 
         # Run the query as normal
         return super().get_results(variable_definitions)
