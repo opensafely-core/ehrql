a76d3d796b5e8dcf396a92dabd9de0803c1b0fb9
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2022-12-02 09:28:08 +0000

    Refactor gentest recording code for clarity

--- tests/generative/recording.py
+++ tests/generative/recording.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 import pprint
 from collections import defaultdict
@@ -47,43 +48,38 @@ def recorder(request):  # pragma: no cover
 
     yield observed_inputs.record
 
-    if os.getenv("GENTEST_COMPREHENSIVE"):
-        operations_seen = {o for v in observed_inputs.variables for o in node_types(v)}
-        variable_strategies.assert_includes_all_operations(operations_seen)
+    if "GENTEST_COMPREHENSIVE" in os.environ:
+        check_comprehensive(observed_inputs)
 
-    capturemanager = request.config.pluginmanager.getplugin("capturemanager")
-    with capturemanager.global_and_fixture_disabled():
-        show_input_summary(observed_inputs)
+    if "GENTEST_DEBUG" in os.environ:
+        with output_enabled(request):
+            show_input_summary(observed_inputs)
 
 
-def histogram(samples):  # pragma: no cover
-    h = defaultdict(int)
-    for sample in samples:
-        h[sample] = h[sample] + 1
-    return sorted(h.items())
+def check_comprehensive(observed_inputs):  # pragma: no cover
+    operations_seen = {o for v in observed_inputs.variables for o in node_types(v)}
+    variable_strategies.assert_includes_all_operations(operations_seen)
 
 
 def show_input_summary(observed_inputs):  # pragma: no cover
-    if "GENTEST_DEBUG" not in os.environ:
-        return
-
     print()
     print(f"\n{len(observed_inputs.unique_inputs)} unique input combinations")
+    show_variables_summary(observed_inputs)
+    show_records_summary(observed_inputs)
 
+
+def show_variables_summary(observed_inputs):  # pragma: no cover
     observed_variables = observed_inputs.variables
     print(f"\n{len(observed_variables)} unique queries")
-
     counts = [count_nodes(example) for example in observed_variables]
     count_histo = histogram(counts)
     print("\nwith this node count distribution")
     for count, num in count_histo:
         print(f"{count:3}\t{num}")
-
     if observed_variables:
         print("\nlargest query")
         by_size = sorted(observed_variables, key=lambda v: count_nodes(v))
         pprint.pprint(by_size[-1])
-
     all_node_types = [
         type_.__name__
         for variable in observed_variables
@@ -94,11 +90,26 @@ def show_input_summary(observed_inputs):  # pragma: no cover
     for type_, num in sorted(type_histo, key=lambda item: item[1], reverse=True):
         print(f"{type_:25}{num}")
 
+
+def show_records_summary(observed_inputs):  # pragma: no cover
     observed_records = observed_inputs.records
     print(f"\n{len(observed_records)} unique datasets")
-
     record_counts = [len(records) for records in observed_records]
     record_count_histo = histogram(record_counts)
     print("\nwith this size distribution")
     for count, num in record_count_histo:
         print(f"{count:3}\t{num}")
+
+
+def histogram(samples):  # pragma: no cover
+    h = defaultdict(int)
+    for sample in samples:
+        h[sample] = h[sample] + 1
+    return sorted(h.items())
+
+
+@contextlib.contextmanager
+def output_enabled(request):  # pragma: no cover
+    capturemanager = request.config.pluginmanager.getplugin("capturemanager")
+    with capturemanager.global_and_fixture_disabled():
+        yield
