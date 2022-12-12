9dbdef91b1e35ec34645e351bb785b0a953b9863
Jon Massey <jon.massey@thedatalab.org>
2022-12-12 14:25:22 +0000

    init tests

--- tests/spec/date_series_ops/test_date_comparisons.py
+++ tests/spec/date_series_ops/test_date_comparisons.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from ..tables import p
 
 title = "Comparisons involving dates"
@@ -128,4 +130,46 @@ def test_is_equal_with_str_date(spec_test):
             3: False,
             4: None,
         },
+    )
+
+
+def test_is_in_dates(spec_test):
+    dates = [date(2010, 1, 1), date(2010, 1, 2)]
+    spec_test(
+        table_data,
+        p.d1.is_in(dates),
+        {
+            1: False,
+            2: False,
+            3: True,
+            4: None,
+        },
+    )
+
+    dates = [date(2010, 1, 2), date(2010, 1, 3)]
+    spec_test(
+        table_data,
+        p.d1.is_in(dates),
+        {
+            1: False,
+            2: False,
+            3: False,
+            4: None,
+        },
+    )
+
+
+def test_is_in_strings(spec_test):
+    d = ["2010-01-01", "2010-01-02"]
+    spec_test(
+        table_data,
+        p.d1.is_in(d),
+        {1: False, 2: False, 3: True, 4: None},
+    )
+
+    d = ["2010-01-02", "2010-01-03"]
+    spec_test(
+        table_data,
+        p.d1.is_in(d),
+        {1: False, 2: False, 3: False, 4: None},
     )
