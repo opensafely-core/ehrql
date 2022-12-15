6649d9e3d3c00ba8a07da42b70c690ec534ab070
David Evans <dave.evans@thedatalab.org>
2022-12-15 16:48:41 +0000

    Increase unit test coverage to 100%
    Because it's easy enough to do.

--- tests/unit/query_model/test_table_schema.py
+++ tests/unit/query_model/test_table_schema.py
@@ -1,3 +1,5 @@
+import datetime
+
 from databuilder.query_model.table_schema import Column, TableSchema
 
 
@@ -8,3 +10,59 @@ def test_table_schema_equality():
     assert t1 == t2
     assert t1 != t3
     assert t1 != "a fish"
+
+
+def test_table_schema_hash():
+    t1 = TableSchema(i=Column(int))
+    t2 = TableSchema(i=Column(int))
+    d = {t1: "hello"}
+    assert d[t2] == "hello"
+
+
+def test_repr_roundtrip():
+    schema = TableSchema(
+        c1=Column(int),
+        c2=Column(datetime.date),
+    )
+
+    assert eval(repr(schema)) == schema
+
+
+def test_from_primitives():
+    t1 = TableSchema.from_primitives(
+        c1=int,
+        c2=str,
+    )
+    t2 = TableSchema(
+        c1=Column(int),
+        c2=Column(str),
+    )
+    assert t1 == t2
+
+
+def test_get_column_type():
+    schema = TableSchema(i=Column(int))
+    assert schema.get_column_type("i") is int
+
+
+def test_column_names():
+    schema = TableSchema(
+        c1=Column(int),
+        c2=Column(datetime.date),
+    )
+    assert schema.column_names == ["c1", "c2"]
+
+
+def test_column_types():
+    schema = TableSchema(
+        c1=Column(int),
+        c2=Column(datetime.date),
+    )
+    assert schema.column_types == [("c1", int), ("c2", datetime.date)]
+
+
+def test_get_column_categories():
+    schema = TableSchema(
+        c1=Column(str, categories=("a", "b", "c")),
+    )
+    assert schema.get_column_categories("c1") == ("a", "b", "c")
