3b1fee433f43721c5349913be579f654dc3fbb34
Jon Massey <jon.massey@thedatalab.org>
2022-12-16 16:37:55 +0000

    add is_not_in() date tests

--- tests/spec/date_series_ops/test_date_comparisons.py
+++ tests/spec/date_series_ops/test_date_comparisons.py
@@ -162,3 +162,34 @@ def test_is_in_strings(spec_test):
             4: None,
         },
     )
+
+
+def test_is_not_in_dates(spec_test):
+    spec_test(
+        table_data,
+        p.d1.is_not_in(
+            [
+                date(2010, 1, 1),
+                date(1900, 1, 1),
+            ]
+        ),
+        {
+            1: True,
+            2: True,
+            3: False,
+            4: None,
+        },
+    )
+
+
+def test_is_not_in_strings(spec_test):
+    spec_test(
+        table_data,
+        p.d1.is_not_in(["2010-01-01", "1900-01-01"]),
+        {
+            1: True,
+            2: True,
+            3: False,
+            4: None,
+        },
+    )
