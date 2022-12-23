f4a53214ca8cfce48389d83b7d9591d82eea8706
David Evans <dave.evans@thedatalab.org>
2022-12-23 15:40:51 +0000

    Refactor generative tests to use new machinery

--- tests/generative/data_setup.py
+++ tests/generative/data_setup.py
@@ -1,70 +1,54 @@
-import sqlalchemy.orm
-
 from databuilder.query_model.nodes import (
     AggregateByPatient,
     Function,
     SelectPatientTable,
     SelectTable,
 )
-from databuilder.utils.orm_utils import orm_class_from_schema
+from databuilder.utils.orm_utils import orm_classes_from_tables
 
 
 def setup(schema, num_patient_tables, num_event_tables):
-    base_class = sqlalchemy.orm.declarative_base()
+    patient_tables = [
+        SelectPatientTable(f"p{i}", schema=schema) for i in range(num_patient_tables)
+    ]
+    event_tables = [
+        SelectTable(f"e{i}", schema=schema) for i in range(num_event_tables)
+    ]
+    all_tables = patient_tables + event_tables
 
-    patient_table_names, patient_classes = _build_orm_classes(
-        "p", num_patient_tables, schema, base_class, has_one_row_per_patient=True
-    )
-    event_table_names, event_classes = _build_orm_classes(
-        "e", num_event_tables, schema, base_class, has_one_row_per_patient=False
-    )
+    orm_classes = orm_classes_from_tables(all_tables)
+    _add_classes_to_module_namespace(orm_classes)
 
-    all_patients_query = _build_query(patient_table_names, event_table_names, schema)
+    patient_classes = [orm_classes[table.name] for table in patient_tables]
+    event_classes = [orm_classes[table.name] for table in event_tables]
+
+    all_patients_query = _build_query(all_tables)
+
+    # We arbitrarily choose the first patient class, but all the ORM classes share the
+    # same MetaData
+    metadata = patient_classes[0].metadata
 
     return (
         patient_classes,
         event_classes,
         all_patients_query,
-        base_class.metadata,
+        metadata,
     )
 
 
-def _build_orm_classes(prefix, count, schema, base_class, has_one_row_per_patient):
-    names = [f"{prefix}{i}" for i in range(count)]
-    classes = [
-        _build_orm_class(name, schema, base_class, has_one_row_per_patient)
-        for name in names
-    ]
-    return names, classes
-
-
-def _build_orm_class(name, schema, base_class, has_one_row_per_patient):
-    class_ = orm_class_from_schema(base_class, name, schema, has_one_row_per_patient)
+def _add_classes_to_module_namespace(orm_classes):
     # It's helpful to have the classes available as module properties so that we can
     # copy-paste failing test cases from Hypothesis. These classes naturally believe
     # that they belong to the `orm_utils` module which created them, so we have to
     # re-parent them here. We use only the final component of the module name as that's
     # how we import it in `test_query_model`.
-    class_.__module__ = __name__.rpartition(".")[2]
-    globals()[class_.__name__] = class_
-    return class_
+    for class_ in orm_classes.values():
+        class_.__module__ = __name__.rpartition(".")[2]
+        globals()[class_.__name__] = class_
 
 
-def _build_query(patient_tables, event_tables, schema):
-    clauses = []
-
-    for table in patient_tables:
-        clauses.append(
-            AggregateByPatient.Exists(
-                source=SelectPatientTable(name=table, schema=schema)
-            )
-        )
-
-    for table in event_tables:
-        clauses.append(
-            AggregateByPatient.Exists(source=SelectTable(name=table, schema=schema))
-        )
-
+def _build_query(tables):
+    clauses = [AggregateByPatient.Exists(source=table) for table in tables]
     return _join_with_or(clauses)
 
 
