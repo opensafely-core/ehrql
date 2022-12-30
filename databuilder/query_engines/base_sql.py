a8d8c3636b3c5a1032450989ac2eb016a337d211
David Evans <dave.evans@thedatalab.org>
2022-12-30 15:01:32 +0000

    Use different approach to enforcing SQLAlchemy dialect
    The `URL` class changes in SQLAlchemy 2.0 such that overriding the
    method like this no longer works.

--- databuilder/query_engines/base_sql.py
+++ databuilder/query_engines/base_sql.py
@@ -544,14 +544,15 @@ class BaseSQLQueryEngine(BaseQueryEngine):
 
     @cached_property
     def engine(self):
-        engine_url = sqlalchemy.engine.make_url(self.dsn)
-        # Hardcode the specific SQLAlchemy dialect we want to use: this is the
-        # dialect the query engine will have been written for and tested with and we
-        # don't want to allow global config changes to alter this
-        engine_url._get_entrypoint = lambda: self.sqlalchemy_dialect
+        # Insert the specific SQLAlchemy dialect we want to use into the dialect
+        # registry: this is the dialect the query engine will have been written for and
+        # tested with and we don't want to allow global config changes to alter this
+        dialect_name = self.sqlalchemy_dialect.__name__
+        sqlalchemy.dialects.registry.impls[dialect_name] = self.sqlalchemy_dialect
+        engine_url = sqlalchemy.engine.make_url(self.dsn).set(drivername=dialect_name)
         engine = sqlalchemy.create_engine(engine_url, future=True)
         # The above relies on abusing SQLAlchemy internals so it's possible it will
-        # break in future -- we want to know immediately if it does
+        # break in future — we want to know immediately if it does
         assert isinstance(engine.dialect, self.sqlalchemy_dialect)
         return engine
 
