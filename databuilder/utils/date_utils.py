6edffdb6b737d9371abb0adaa281781b8b713253
Becky Smith <becky.smith@thedatalab.org>
2023-01-05 16:28:42 +0000

    Implement suggested refactoring in date_utils

--- databuilder/utils/date_utils.py
+++ databuilder/utils/date_utils.py
@@ -38,8 +38,9 @@ def date_add_days(date, num_days):
     return date + datetime.timedelta(days=num_days)
 
 
-def valid_year(year):
-    return 0 < year <= 9999
+def assert_valid_year(year):
+    if not 0 < year <= 9999:
+        raise ValueError(f"year {year} is out of range")
 
 
 def date_add_months(date, num_months):
@@ -48,36 +49,33 @@ def date_add_months(date, num_months):
     new_zero_indexed_month = zero_indexed_month + num_months
     new_month = 1 + new_zero_indexed_month % 12
     new_year = date.year + new_zero_indexed_month // 12
+    assert_valid_year(new_year)
     try:
         return datetime.date(new_year, new_month, date.day)
     except ValueError:
-        # We should only ever get an error for:
-        # 1) An out-of-range year, if new_year results in a value < 1 or > 9999 OR
-        # 2) A new month which has no corresponding day; in this case we roll forward to the
-        #    first of the next month. For a defence of this logic see:
-        #    tests/spec/date_series/ops/test_date_series_ops.py::test_add_months
+        # We should only ever get an error for a new month which has no corresponding day;
+        # in this case we roll forward to the first of the next month.
+        # For a defence of this logic see:
+        # tests/spec/date_series/ops/test_date_series_ops.py::test_add_months
 
         # As no month has more days than December we'll never need to roll forward from
         # December and so we don't need to worry about wrapping round to the next year
         assert new_month != 12
-        if valid_year(new_year):
-            return datetime.date(new_year, new_month + 1, 1)
-        raise
+        return datetime.date(new_year, new_month + 1, 1)
 
 
 def date_add_years(date, num_years):
     new_year = date.year + num_years
+    assert_valid_year(new_year)
     try:
         return datetime.date(new_year, date.month, date.day)
     except ValueError:
-        # We should only ever get an error for:
-        # 1) An out-of-range year, if new_year results in a value < 1 or > 9999 OR
-        # 2) 29 Feb on non-leap years, which we want to roll forward to 1 Mar.
-        #    For a defence of this logic see:
-        #    tests/spec/date_series/ops/test_date_series_ops.py::test_add_years
-        if date.month == 2 and date.day == 29 and valid_year(new_year):
-            return datetime.date(date.year + num_years, 3, 1)
-        raise
+        # We should only ever get an error for 29 Feb on non-leap years, which we want to roll
+        # forward to 1 Mar.
+        # For a defence of this logic see:
+        # tests/spec/date_series/ops/test_date_series_ops.py::test_add_years
+        assert date.month == 2 and date.day == 29
+        return datetime.date(date.year + num_years, 3, 1)
 
 
 def to_first_of_year(date):
