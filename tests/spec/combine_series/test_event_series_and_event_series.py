13b7e68dd5955351dfe29a2e44050ae08781187c
Becky Smith <becky.smith@thedatalab.org>
2023-01-09 10:13:10 +0000

    fix: Make the in-memory check on Row keys independent of key order

--- tests/spec/combine_series/test_event_series_and_event_series.py
+++ tests/spec/combine_series/test_event_series_and_event_series.py
@@ -4,12 +4,12 @@ title = "Combining two event series"
 
 table_data = {
     e: """
-          |  i1 |  i2
-        --+-----+-----
-        1 | 101 | 111
-        1 | 102 | 112
-        2 | 201 | 211
-        2 | 202 | 212
+          |  i1 |  i2 | s1
+        --+-----+-----+---
+        1 | 101 | 111 | b
+        1 | 102 | 112 | a
+        2 | 201 | 211 | b
+        2 | 202 | 212 | a
     """,
 }
 
@@ -21,5 +21,19 @@ def test_event_series_and_event_series(spec_test):
         {
             1: (101 + 111) + (102 + 112),
             2: (201 + 211) + (202 + 212),
+        },
+    )
+
+
+def test_event_series_and_sorted_event_series(spec_test):
+    """
+    The sort order of the underlying event series does not affect their combination.
+    """
+    spec_test(
+        table_data,
+        (e.i1 + e.sort_by(e.s1).i2).minimum_for_patient(),
+        {
+            1: (101 + 111),
+            2: (201 + 211),
         },
     )
