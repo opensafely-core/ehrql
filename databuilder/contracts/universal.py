ffbed806b6b081751e56468e22a271c70cc13d67
David Evans <dave.evans@thedatalab.org>
2023-01-25 13:02:44 +0000

    Remove redundant and/or misleading descriptions
    In some cases these were just repeating information specified in the
    constraints, and we already render the constraints to text in the
    documentation.
    
    References to the formatting are misleading: these are dates not
    strings; they don't have a format until we decide to render them as
    strings.

--- databuilder/contracts/universal.py
+++ databuilder/contracts/universal.py
@@ -10,15 +10,12 @@ class patients(PatientFrame):
 
     date_of_birth = Series(
         datetime.date,
-        description=(
-            "Patient's year and month of birth, provided in format YYYY-MM-01. "
-            "The day will always be the first of the month."
-        ),
+        description="Patient's date of birth, rounded to first of month",
         constraints=[Constraint.FirstOfMonth(), Constraint.NotNull()],
     )
     sex = Series(
         str,
-        description="Patient's sex as defined by the options: male, female, intersex, unknown.",
+        description="Patient's sex",
         implementation_notes_to_add_to_description=(
             'Specify how this has been determined, e.g. "sex at birth", or "current sex".'
         ),
@@ -29,7 +26,5 @@ class patients(PatientFrame):
     )
     date_of_death = Series(
         datetime.date,
-        description=(
-            "Patient's year and month of death, provided in format YYYY-MM-01. "
-        ),
+        description="Patient's date of death",
     )
