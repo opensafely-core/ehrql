d05473aa0098d27799279ebf2a5d9beabd1241e4
David Evans <dave.evans@thedatalab.org>
2023-02-02 17:06:20 +0000

    fix: Handle `None` value in query rewriter

--- tests/unit/query_model/test_query_graph_rewriter.py
+++ tests/unit/query_model/test_query_graph_rewriter.py
@@ -1,4 +1,5 @@
 from databuilder.query_model.nodes import (
+    Case,
     Column,
     Filter,
     Function,
@@ -45,3 +46,22 @@ def test_query_graph_rewriter_handles_nodes_in_frozensets():
 
     new_graph = rewriter.rewrite(graph)
     assert new_graph == {"filters": frozenset([filter_3, filter_2])}
+
+
+def test_query_graph_rewriter_handles_case_without_default():
+    events = SelectTable("events", schema=TableSchema(i=Column(int)))
+    col_i = SelectColumn(events, "i")
+    is_positive = Function.GE(col_i, Value(0))
+    is_negative = Function.LT(col_i, Value(0))
+
+    graph = {
+        "case": Case({is_positive: Value("valid")}, default=None),
+    }
+
+    rewriter = QueryGraphRewriter()
+    rewriter.replace(is_positive, is_negative)
+
+    new_graph = rewriter.rewrite(graph)
+    assert new_graph == {
+        "case": Case({is_negative: Value("valid")}, default=None),
+    }
