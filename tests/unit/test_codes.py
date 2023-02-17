94cccac3b7566d3c4567d802bd19c7537d92124e
David Evans <dave.evans@thedatalab.org>
2023-02-17 12:24:29 +0000

    Validate the format of codes

--- tests/unit/test_codes.py
+++ tests/unit/test_codes.py
@@ -2,7 +2,16 @@ import textwrap
 
 import pytest
 
-from databuilder.codes import CodelistError, CTV3Code, codelist_from_csv
+from databuilder.codes import (
+    BNFCode,
+    CodelistError,
+    CTV3Code,
+    DMDCode,
+    ICD10Code,
+    OPCS4Code,
+    SNOMEDCTCode,
+    codelist_from_csv,
+)
 
 
 def test_codelist_from_csv(tmp_path):
@@ -61,3 +70,57 @@ def test_codelist_from_csv_with_categories(tmp_path):
         CTV3Code("def00"): "bar",
         CTV3Code("ghi00"): "",
     }
+
+
+@pytest.mark.parametrize(
+    "cls,value",
+    [
+        (BNFCode, "0101010I0AAAEAE"),
+        (BNFCode, "23965909711"),
+        (CTV3Code, "ABC01"),
+        (CTV3Code, "De4.."),
+        (ICD10Code, "A01"),
+        (ICD10Code, "A012"),
+        (OPCS4Code, "B23"),
+        (OPCS4Code, "B234"),
+        (SNOMEDCTCode, "1234567890"),
+    ],
+)
+def test_valid_codes(cls, value):
+    assert cls(value).value == value
+
+
+@pytest.mark.parametrize(
+    "cls,value",
+    [
+        # Digit (5) instead of letter as first character of Product
+        (BNFCode, "0101010I05AAEAE"),
+        # Appliance but too many digits
+        (BNFCode, "239659097111"),
+        # Wrong length
+        (CTV3Code, "ABC0"),
+        # Dot other than at the end
+        (CTV3Code, "ABC.0"),
+        # Letter other than at the start
+        (ICD10Code, "AA1"),
+        # Wrong length
+        (ICD10Code, "A0124"),
+        # I is not an allowed first character
+        (OPCS4Code, "I00"),
+        # Too short
+        (SNOMEDCTCode, "123"),
+        # Too long
+        (SNOMEDCTCode, "12345678901234567890"),
+        # Leading zero
+        (SNOMEDCTCode, "0123456789"),
+    ],
+)
+def test_invalid_codes(cls, value):
+    with pytest.raises(ValueError):
+        cls(value)
+
+
+def test_syntactically_equivalent_codes():
+    # No point duplicating the tests here, but we'll need to test them if we ever stop
+    # sharing the regex
+    assert DMDCode.regex == SNOMEDCTCode.regex
