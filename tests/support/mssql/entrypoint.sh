25b89036bcf97246b329582b1df6e993c34a1633
David Evans <dave.evans@thedatalab.org>
2023-03-24 10:33:59 +0000

    Work around race condition in setting MSSQL collation
    Previously, if our database setup script managed to run before the
    collation setting had been applied the tests would fail. Mostly this
    didn't happen but sometimes it did. This was fun only in the Data
    Connection sense of the word.

--- tests/support/mssql/entrypoint.sh
+++ tests/support/mssql/entrypoint.sh
@@ -15,22 +15,46 @@ if [ "$1" = '/opt/mssql/bin/sqlservr' ]; then
     function initialize_app_database() {
         timeout=20
         limit="$((SECONDS + timeout))"
-        # Run the setup script to create the DB and the schema in the DB
-        until /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -d master -i /mssql/setup.sql; do
-          # Wait a bit for SQL Server to start. SQL Server's process
-          # doesn't provide a clever way to check if it's up or not, and
-          # it needs to be up before we can import the application
-          # database
+
+        # MSSQL doesn't apply the default collation specified in the
+        # MSSQL_COLLATION environment variable immediately on startup. So we
+        # need to wait for this to be applied before we create our application
+        # databases, otherwise we have a race-condition. We check the collation
+        # of "tempdb" as that's the system database whose collation affects us.
+        until [[ "$(get_tempdb_collation)" == "$MSSQL_COLLATION" ]]; do
           sleep 1s
           if [[ "${SECONDS}" -gt "${limit}" ]]; then
-            echo >&2 "Failed to connect to mssql after ${timeout} seconds"
+            echo >&2 "Failed to connect to MSSQL and confirm collation setting after ${timeout} seconds"
+            echo >&2 "Expected collation: $MSSQL_COLLATION"
+            echo >&2 "Got: $(get_tempdb_collation)"
             exit 1
           fi
         done
+
+        # Run the setup script to create the DB and the schema in the DB
+        _sqlcmd -i /mssql/setup.sql
+
         # Note that the container has been initialized so future
         # starts won't wipe changes to the data
         touch /tmp/app-initialized
     }
+
+    function get_tempdb_collation() {
+      # We need to set NOCOUNT so we don't get a "Rows affected" count in the output
+      _sqlcmd -Q "SET NOCOUNT ON; SELECT DATABASEPROPERTYEX('tempdb', 'Collation');"
+    }
+
+    function _sqlcmd() {
+      # Extra arguments:
+      #    -b : exit with 1 on error rather than 0
+      # -h -1 : no headers in output
+      #    -W : trim trailing whitespace
+      /opt/mssql-tools/bin/sqlcmd \
+        -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -d master \
+        -b -h -1 -W \
+        "$@"
+    }
+
     initialize_app_database &
   fi
 fi
