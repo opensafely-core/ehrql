6d712706cbc91ce4ca78cfcd0441e18973c97457
David Evans <dave.evans@thedatalab.org>
2023-03-29 12:56:55 +0100

    Remove `pysqlite-binary` package
    The original reason for using this package was that some developers
    (particularly those on macOS) had a version of SQLite compiled without
    the maths extensions which provide the `FLOOR()` function.
    
    Starting with Python 3.11, the version of SQLite bundled with the macOS
    and Windows installers now includes the maths extensions, so this
    specific reason no longer applies.
    
    There are some advantages to using an externally provided SQLite whose
    version is controlled by us, however:
    
     * the [`pysqlite-binary`][1] package we were using appears to be
       unmaintained and has no Python 3.11 wheel;
     * the [`pysqlite3`][2] package on which it is based has no plans to
       provide macOS wheels;
     * everything is terrible and we cannot have nice things.
    
    Reverting back to stdlib SQLite resolves the problem for now.
    
    It's possible that if you're developing on an old Linux distribution you
    might have a SQLite without maths extenions. But those using old Linux
    distributions can generally look after themselves.
    
    [1]: https://github.com/pysqlite3/pysqlite3
    [2]: https://github.com/coleifer/pysqlite3

--- databuilder/query_engines/sqlite_dialect.py
+++ databuilder/query_engines/sqlite_dialect.py
@@ -1,14 +1,8 @@
-import pysqlite3
 from sqlalchemy.dialects.sqlite.pysqlite import SQLiteDialect_pysqlite
 
 
 class SQLiteDialect(SQLiteDialect_pysqlite):
     supports_statement_cache = True
-
-    @classmethod
-    def import_dbapi(cls):
-        # Use pysqlite3's bundled SQLite rather than the system version
-        return pysqlite3.dbapi2
 
     def do_on_connect(self, connection):
         # Set the per-connection flag which makes LIKE queries case-sensitive
