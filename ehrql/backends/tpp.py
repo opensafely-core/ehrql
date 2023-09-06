3c73c04504b96f9e7dadd9727cd36fb9cc561974
Becky Smith <becky.smith@thedatalab.org>
2023-09-06 09:17:35 +0100

    Use NULLIF to replace ceiling dates

--- ehrql/backends/tpp.py
+++ ehrql/backends/tpp.py
@@ -89,10 +89,7 @@ class TPPBackend(BaseBackend):
         """
             SELECT
                 Patient_ID AS patient_id,
-                CASE
-                    WHEN ConsultationDate = '9999-12-31T00:00:00' THEN NULL
-                    ELSE CAST(ConsultationDate AS date)
-                END AS date,
+                CAST(NULLIF(ConsultationDate, '9999-12-31T00:00:00') AS date) AS date,
                 NULL AS snomedct_code,
                 CTV3Code AS ctv3_code,
                 NumericValue AS numeric_value
@@ -100,10 +97,7 @@ class TPPBackend(BaseBackend):
             UNION ALL
             SELECT
                 Patient_ID AS patient_id,
-                CASE
-                    WHEN ConsultationDate = '9999-12-31T00:00:00' THEN NULL
-                    ELSE CAST(ConsultationDate AS date)
-                END AS date,
+                CAST(NULLIF(ConsultationDate, '9999-12-31T00:00:00') AS date) AS date,
                 ConceptId AS snomedct_code,
                 NULL AS ctv3_code,
                 NumericValue AS numeric_value
