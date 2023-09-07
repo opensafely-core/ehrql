7e1cd2faf0cf0195dfb7397635191e24301e63a3
Steven Maude <git@stevenmaude.co.uk>
2023-09-07 14:35:11 +0100

    Reformat birth_date-standalone-success example
    To ignore Ruff and Black.

--- docs/includes/code/reference/language/birth_date-standalone-success/analysis/dataset_definition.py
+++ docs/includes/code/reference/language/birth_date-standalone-success/analysis/dataset_definition.py
@@ -1,14 +1,10 @@
 from ehrql import Dataset
-
 # --8<-- [start:import]
 from ehrql.tables.beta.core import patients
-
-
 # --8<-- [end:import]
 
 # --8<-- [start:date_of_birth_column]
 dob = patients.date_of_birth
 # --8<-- [end:date_of_birth_column]
-
 dataset = Dataset()
 dataset.define_population(dob.year < 2000)
