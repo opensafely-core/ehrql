419dd407b320130251077d047d181182a03a4ffd
David Evans <dave.evans@thedatalab.org>
2023-09-21 15:05:16 +0100

    Dynamically generate list of modules of to test
    This ensures that we don't miss any in future.
    
    We also use the name of the bound value in the module, rather than the
    class name so as to correctly handle aliased imports.

--- tests/unit/test_tables.py
+++ tests/unit/test_tables.py
@@ -1,16 +1,16 @@
 import pytest
 
+import ehrql.tables
 from ehrql.query_language import BaseFrame
-from ehrql.tables.beta import tpp
-from ehrql.tables.examples import tutorial
+from ehrql.utils.module_utils import get_submodules
 
 
-@pytest.mark.parametrize("module", [tpp, tutorial])
+@pytest.mark.parametrize("module", list(get_submodules(ehrql.tables)))
 def test___all__(module):
-    assert module.__all__ == sorted(module.__all__)
     table_names = {
-        type(obj).__name__
-        for obj in vars(module).values()
-        if isinstance(obj, BaseFrame)
+        name for name, obj in vars(module).items() if isinstance(obj, BaseFrame)
     }
+    if not table_names:
+        pytest.skip(f"{module.__name__} has no tables")
+    assert module.__all__ == sorted(module.__all__)
     assert table_names == set(module.__all__)
