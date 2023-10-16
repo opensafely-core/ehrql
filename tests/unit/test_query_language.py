2da351d9710977083d792fede62cd2f6940ba49d
David Evans <dave.evans@thedatalab.org>
2023-10-16 07:37:06 +0100

    Cover code previously only covered in dataset definitions
    As dataset definition files are now executed in subprocesses they no
    longer count for coverage purposes. Possibly we could fix this, but I
    think it's simpler to just separately cover the tiny handful of missing
    lines.

--- tests/unit/test_query_language.py
+++ tests/unit/test_query_language.py
@@ -82,8 +82,10 @@ def test_dataset():
     dataset = Dataset()
     dataset.define_population(year_of_birth <= 2000)
     dataset.year_of_birth = year_of_birth
+    dataset.configure_dummy_data(population_size=123)
 
     assert dataset.year_of_birth is year_of_birth
+    assert dataset.dummy_data_config.population_size == 123
 
     assert compile(dataset) == {
         "year_of_birth": Function.YearFromDate(
