7167fe2793cb3b65e81567ce194b29e753cf3dfe
David Evans <dave.evans@thedatalab.org>
2024-02-29 15:58:37 +0000

    Simplify `LocalFileQueryEngine` slightly

--- ehrql/query_engines/local_file.py
+++ ehrql/query_engines/local_file.py
@@ -12,33 +12,26 @@ class LocalFileQueryEngine(InMemoryQueryEngine):
     Subclass of the in-memory engine which loads its data from files
     """
 
-    def __init__(self, dsn, *args, **kwargs):
-        # Treat the DSN as the path to a directory of table files
-        self.data_directory = Path(dsn)
-        # The in-memory engine is a bit unusual in that it expects the DSN to be an
-        # actual instance of the database
-        dsn = InMemoryDatabase()
-        super().__init__(dsn, *args, **kwargs)
+    database = None
 
     def get_results(self, variable_definitions):
         # Given the variables supplied determine the tables used and load the associated
         # data into the database
-        table_nodes = get_table_nodes(*variable_definitions.values())
-        self.populate_database(table_nodes)
-
+        self.populate_database(
+            get_table_nodes(*variable_definitions.values()),
+        )
         # Run the query as normal
         return super().get_results(variable_definitions)
 
     def populate_database(self, table_nodes):
-        # Populate the database using the files in the supplied directory
         table_specs = {
             table.name: get_column_specs_from_schema(table.schema)
             for table in table_nodes
         }
         table_rows = read_tables(
-            self.data_directory,
+            Path(self.dsn),
             table_specs,
             allow_missing_columns=True,
         )
         table_data = dict(zip(table_nodes, table_rows))
-        self.database.populate(table_data)
+        self.database = InMemoryDatabase(table_data)
