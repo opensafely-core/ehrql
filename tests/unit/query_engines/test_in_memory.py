bf01cee00bf82733886468e605bce2f31d536b19
David Evans <dave.evans@thedatalab.org>
2024-03-01 09:39:09 +0000

    Remove use of `setup()` on `InMemoryDatabase`
    This now lives on the test wrapper, not on the database class itself.

--- tests/unit/query_engines/test_in_memory.py
+++ tests/unit/query_engines/test_in_memory.py
@@ -3,7 +3,6 @@ from datetime import date
 from ehrql.query_engines.in_memory import InMemoryQueryEngine
 from ehrql.query_engines.in_memory_database import InMemoryDatabase
 from ehrql.query_language import EventFrame, Series, table
-from ehrql.utils.orm_utils import make_orm_models
 
 
 @table
@@ -14,8 +13,13 @@ class events(EventFrame):
 def test_pick_one_row_per_patient():
     # This test verifies that picking one row per patient works without first having
     # applied QM transformations to all variables in a dataset.
-    database = InMemoryDatabase()
-    database.setup(make_orm_models({events: [{"patient_id": 1, "date": "2023-01-01"}]}))
+    database = InMemoryDatabase(
+        {
+            events._qm_node: [
+                (1, date(2023, 1, 1)),
+            ],
+        }
+    )
     engine = InMemoryQueryEngine(database)
     engine.cache = {}
     frame = events.sort_by(events.date).first_for_patient()
