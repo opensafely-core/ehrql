547ae16a7932ae4f4e3e89f62945fd6275e019bb
David Evans <dave.evans@thedatalab.org>
2024-04-24 17:49:30 +0100

    Enforce consistent docstrings for inherited tables
    Docstrings are a bit of a wrinkle here: they obviously aren't
    automatically inherited from the parent; we could set them dynamically
    by fiddling about with the `__doc__` attribute but that would prevent
    them showing up in VSCode, and it's quite a nice feature that they
    display directly in the editor.
    
    So instead we require them to be manually copy-pasted but we add a check
    that they are consistent to avoid drift. We don't require that they are
    identical because it seems reasonable to allow the child table to extend
    the parent docstring, but they must share a common prefix.

---
+++ tests/unit/docs/test_schemas.py
@@ -0,0 +1,41 @@
+import pytest
+
+from ehrql.docs.schemas import get_table_docstring
+from ehrql.tables import EventFrame, Series, table
+
+
+def test_get_table_docstring():
+    @table
+    class parent_table(EventFrame):
+        "I have a docstring"
+        col_a = Series(str)
+
+    @table
+    class child_table(parent_table.__class__):
+        """
+        I have a docstring
+
+        With some extra stuff
+        """
+
+        col_b = Series(str)
+
+    assert (
+        get_table_docstring(child_table.__class__)
+        == "I have a docstring\n\nWith some extra stuff"
+    )
+
+
+def test_get_table_docstring_with_mismatch():
+    @table
+    class parent_table(EventFrame):
+        "I have a docstring"
+        col_a = Series(str)
+
+    @table
+    class child_table(parent_table.__class__):
+        "I have a different docstring"
+        col_b = Series(str)
+
+    with pytest.raises(ValueError):
+        get_table_docstring(child_table.__class__)
