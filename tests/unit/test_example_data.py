9a3910902ae5bc1c4f5100cbb2462a997af469dd
David Evans <dave.evans@thedatalab.org>
2024-10-23 16:31:13 +0100

    Correct type annotation
    The example tables contains a mixture of `PatientFrame` and `EventFrame`
    instances.

--- tests/unit/test_example_data.py
+++ tests/unit/test_example_data.py
@@ -4,7 +4,7 @@ import pytest
 
 import ehrql
 from ehrql.query_engines.local_file import LocalFileQueryEngine
-from ehrql.query_language import EventFrame
+from ehrql.query_language import BaseFrame
 from ehrql.tables import core, tpp
 
 
@@ -18,7 +18,7 @@ EXAMPLE_DATA_DIR = Path(ehrql.__file__).parent / "example-data"
 
 
 @pytest.mark.parametrize("ql_table", EXAMPLE_TABLES)
-def test_populate_database_using_example_data(ql_table: EventFrame):
+def test_populate_database_using_example_data(ql_table: BaseFrame):
     # The engine populates the database with the example data and validates the column specs in the process
     engine = LocalFileQueryEngine(EXAMPLE_DATA_DIR)
     engine.populate_database([ql_table._qm_node])
