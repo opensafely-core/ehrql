babe1260278d99310c0aa09ed8414b88c62c4883
Alice Wong <alice.wong@thedatalab.org>
2024-11-08 12:17:18 +0000

    Add quiz module
    Copy and pasted the current state of aw/quiz-checking. This commit should be updated accordingly with changes in that branch.

---
+++ ehrql/quiz.py
@@ -0,0 +1,211 @@
+from typing import Any
+
+from ehrql.query_engines.in_memory_database import (
+    EventColumn,
+    EventTable,
+    PatientColumn,
+    PatientTable,
+)
+from ehrql.query_engines.sandbox import EmptyDataset, SandboxQueryEngine
+from ehrql.query_language import BaseFrame, BaseSeries, Dataset
+
+
+def check_answer(
+    engine: SandboxQueryEngine, answer: Any, expected: Dataset | BaseFrame | BaseSeries
+) -> str:
+    message = check_type(answer, expected)
+    if message:
+        return message
+
+    ev_answer = evaluate(engine, answer)
+    ev_expected = evaluate(engine, expected)
+
+    message = check_type(ev_answer, ev_expected)
+    if message:
+        return message
+
+    if ev_answer == ev_expected:
+        return "Correct!"
+
+    checks = [
+        check_dataset_not_empty,
+        check_dataset_columns,
+        check_patient_ids,
+        check_patient_table_values,
+        check_patient_column_values,
+        check_event_row_ids,
+        check_event_table_values,
+        check_event_column_values,
+    ]
+
+    for check in checks:
+        message = check(ev_answer, ev_expected)
+        if message is None:
+            continue  # to the next check
+        return message
+    return "\n".join(
+        [
+            "Incorrect answer.",
+            "Expected:",
+            str(ev_expected),
+            "Got:",
+            str(ev_answer),
+        ]
+    )
+
+
+def get_type_name(obj: Any) -> str:
+    # Simplify the type name for better error messages
+    if isinstance(obj, BaseFrame):
+        return "Table"
+    elif isinstance(obj, BaseSeries):
+        return "Series"
+    return type(obj).__name__
+
+
+def check_type(answer: Any, expected: Any) -> str:
+    if not isinstance(answer, type(expected)):
+        answer_type_name = get_type_name(answer)
+        expected_type_name = get_type_name(expected)
+
+        if answer_type_name == "EmptyDataset":
+            return None  # Return a different message for empty datasets
+        if answer_type_name != expected_type_name:
+            # Only return an error message if it is helpful
+            return f"Expected {expected_type_name}, got {answer_type_name} instead."
+    return None
+
+
+def evaluate(
+    engine: SandboxQueryEngine, answer: Dataset | BaseFrame | BaseSeries
+) -> Any:
+    if isinstance(answer, Dataset):
+        return engine.evaluate_dataset(answer)
+    return engine.evaluate(answer)
+
+
+def check_dataset_not_empty(ev_ans: Any, ev_exp: Any) -> str | None:
+    if isinstance(ev_ans, EmptyDataset):
+        if isinstance(ev_exp, EmptyDataset):
+            return "Correct!"  # Special case: Not an error
+        return "The dataset is empty."
+    return None
+
+
+def check_dataset_columns(ev_ans: Any, ev_exp: Any) -> str | None:
+    # Named so because we do not expect PatientTables from Frames to have varying columns
+    if isinstance(ev_exp, PatientTable):
+        return _check_missing_extra(
+            ev_ans, ev_exp, "column", getter=lambda t: t.name_to_col.keys()
+        )
+    return None
+
+
+def check_patient_ids(ev_ans: Any, ev_exp: Any) -> str | None:
+    if isinstance(ev_exp, PatientTable) or isinstance(ev_exp, PatientColumn):
+        return _check_missing_extra(
+            ev_ans, ev_exp, "patient", getter=lambda t: t.patients()
+        )
+    return None
+
+
+def check_patient_table_values(ev_ans: Any, ev_exp: Any) -> str | None:
+    if isinstance(ev_exp, PatientTable):
+        return _check_columns_one_by_one(
+            ev_ans,
+            ev_exp,
+            check_patient_column_values,
+            column_names=list(ev_exp.name_to_col.keys() - {"patient_id"}),
+            # Patient ID handled separately
+        )
+    return None
+
+
+def check_patient_column_values(
+    ev_ans, ev_exp, column_name: str | None = None
+) -> str | None:
+    if isinstance(ev_exp, PatientColumn):
+        column_name = f" `{column_name}` " if column_name else " "
+        incorrect = sorted(
+            ev_ans.patient_to_value.items() - ev_exp.patient_to_value.items()
+        )
+        for k, v in incorrect:  # Only show the first incorrect value
+            return f"Incorrect{column_name}value for patient {k}: expected {str(ev_exp[k])}, got {str(v)} instead."
+    return None
+
+
+def check_event_row_ids(ev_ans: Any, ev_exp: Any) -> str | None:
+    if isinstance(ev_exp, EventTable) or isinstance(ev_exp, EventColumn):
+        return _check_missing_extra(
+            ev_ans,
+            ev_exp,
+            "row",
+            getter=lambda t: set(row["row_id"] for row in t.to_records()),
+        )
+
+    return None
+
+
+def check_event_table_values(ev_ans: Any, ev_exp: Any) -> str | None:
+    if isinstance(ev_exp, EventTable):
+        return _check_columns_one_by_one(
+            ev_ans,
+            ev_exp,
+            check_event_column_values,
+            column_names=list(ev_exp.name_to_col.keys() - {"patient_id", "row_id"}),
+            # Patient ID and Row ID handled separately
+        )
+    return None
+
+
+def check_event_column_values(
+    ev_ans, ev_exp, column_name: str | None = None
+) -> str | None:
+    if isinstance(ev_exp, EventColumn):
+        column_name = f" `{column_name}` " if column_name else " "
+        records_ans = set(tuple(rec.values()) for rec in ev_ans.to_records())
+        records_exp = set(tuple(rec.values()) for rec in ev_exp.to_records())
+        incorrect = sorted(records_ans - records_exp)
+        for p, r, v in incorrect:  # Only show the first incorrect value
+            return f"Incorrect{column_name}value for patient {p}, row {r}: expected {str(ev_exp[p][r])}, got {str(v)} instead."
+    return None
+
+
+# Utils functions
+def get_items_missing_extra(
+    set_ans: set,
+    set_exp: set,
+    item_name: str,
+) -> tuple[str | None, str | None]:
+    missing = list(map(str, sorted(set_exp - set_ans)))
+    missing = f"Missing {item_name}(s): {', '.join(missing)}." if missing else None
+    extra = list(map(str, sorted(set_ans - set_exp)))
+    extra = f"Found extra {item_name}(s): {', '.join(extra)}." if extra else None
+    return missing, extra
+
+
+def _check_missing_extra(
+    ev_ans: Any,
+    ev_exp: Any,
+    item_name: str,
+    getter: callable,
+) -> str | None:
+    missing_columns, extra_columns = get_items_missing_extra(
+        getter(ev_ans), getter(ev_exp), item_name
+    )
+    if missing_columns or extra_columns:
+        return "\n".join(filter(None, [missing_columns, extra_columns]))
+    return None
+
+
+def _check_columns_one_by_one(
+    ev_ans: Any,
+    ev_exp: Any,
+    check_column_values: callable,
+    column_names: list[str],
+) -> str | None:
+    for name in column_names:
+        msg = check_column_values(ev_ans[name], ev_exp[name], name)
+        if msg is None:
+            continue
+        return msg
---
+++ tests/unit/test_quiz.py
@@ -0,0 +1,213 @@
+from pathlib import Path
+
+import pytest
+
+from ehrql import quiz
+from ehrql.query_engines.sandbox import SandboxQueryEngine
+from ehrql.query_language import Dataset
+from ehrql.tables.core import (
+    clinical_events,
+    medications,
+    patients,
+    practice_registrations,
+)
+
+
+@pytest.fixture
+def engine():
+    path = Path(__file__).parents[2] / "ehrql" / "example-data"
+    return SandboxQueryEngine(str(path))
+
+
+def dataset_smoketest(
+    index_year=2022, min_age=18, max_age=80, year_of_birth_column: bool = False
+) -> Dataset:
+    year_of_birth = patients.date_of_birth.year
+    age = index_year - year_of_birth
+
+    dataset = Dataset()
+    dataset.define_population((age >= min_age) & (age <= max_age))
+    dataset.age = age
+    if year_of_birth_column:
+        dataset.year_of_birth = patients.date_of_birth.year
+    return dataset
+
+
+@pytest.mark.parametrize(
+    "answer, expected, message",
+    [
+        (1, Dataset(), "Expected Dataset, got int instead."),
+        (patients, Dataset(), "Expected Dataset, got Table instead."),
+        (patients.date_of_birth, Dataset(), "Expected Dataset, got Series instead."),
+        (Dataset(), patients, "Expected Table, got Dataset instead."),
+        (patients, patients.date_of_birth, "Expected Series, got Table instead."),
+    ],
+)
+def test_wrong_type_before_evaluation(answer, expected, message):
+    msg = quiz.check_answer(engine=None, answer=answer, expected=expected)
+    assert msg == message
+
+
+def test_event_frame_not_converted_to_patient_frame(engine):
+    # Wrong type after evaluation
+    msg = quiz.check_answer(
+        engine=engine,
+        answer=clinical_events,
+        expected=clinical_events.sort_by(clinical_events.date).last_for_patient(),
+    )
+    assert msg == "Expected PatientTable, got EventTable instead."
+
+
+@pytest.mark.parametrize(
+    "answer, expected",
+    [
+        (Dataset(), Dataset()),  # Same syntax but different objects
+        (dataset_smoketest(), dataset_smoketest()),
+        (patients, patients),
+        (patients.date_of_birth, patients.date_of_birth),
+        (clinical_events, clinical_events),
+        (medications.dmd_code, medications.dmd_code),
+    ],
+)
+def test_same_syntax_correct(engine, answer, expected):
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    assert msg == "Correct!"
+
+
+def test_empty_dataset(engine):
+    msg = quiz.check_answer(
+        engine=engine, answer=Dataset(), expected=dataset_smoketest()
+    )
+    assert msg == "The dataset is empty."
+
+
+@pytest.mark.parametrize(
+    "order, message",
+    [
+        ([0, 1], "Missing column(s): year_of_birth."),
+        ([1, 0], "Found extra column(s): year_of_birth."),
+    ],
+)
+def test_dataset_has_missing_or_extra_column(engine, order, message):
+    datasets = [dataset_smoketest(), dataset_smoketest(year_of_birth_column=True)]
+    answer, expected = (datasets[i] for i in order)
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    assert msg == message
+
+
+def test_dataset_typo_in_column_name(engine):
+    answer = dataset_smoketest(year_of_birth_column=False)
+    answer.yeah_of_birth = patients.date_of_birth.year
+    expected = dataset_smoketest(year_of_birth_column=True)
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    assert (
+        msg
+        == "Missing column(s): year_of_birth.\nFound extra column(s): yeah_of_birth."
+    )
+
+
+@pytest.mark.parametrize(
+    "order, message",
+    [
+        ([0, 1], "Missing patient(s): 4, 5, 9."),
+        ([1, 0], "Found extra patient(s): 4, 5, 9."),
+    ],
+)
+def test_dataset_has_missing_or_extra_patients(engine, order, message):
+    datasets = [dataset_smoketest(), dataset_smoketest(min_age=0, max_age=100)]
+    answer, expected = (datasets[i] for i in order)
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    assert msg == message
+
+
+def test_dataset_has_incorrect_value(engine):
+    msg = quiz.check_answer(
+        engine=engine,
+        answer=dataset_smoketest(index_year=2023),
+        expected=dataset_smoketest(),
+    )
+    assert msg == "Incorrect `age` value for patient 1: expected 49, got 50 instead."
+
+
+@pytest.mark.parametrize(
+    "order, message",
+    [
+        ([0, 1], "Missing patient(s): 7."),
+        ([1, 0], "Found extra patient(s): 7."),
+    ],
+)
+def test_patient_series_has_missing_or_extra_patients(engine, order, message):
+    series = [
+        practice_registrations.for_patient_on("2013-12-01").practice_pseudo_id,
+        practice_registrations.for_patient_on("2014-01-01").practice_pseudo_id,
+    ]
+    answer, expected = (series[i] for i in order)
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    assert msg == message
+
+
+def test_patient_series_has_incorrect_value(engine):
+    msg = quiz.check_answer(
+        engine=engine,
+        answer=patients.age_on("2023-12-31"),
+        expected=patients.age_on("2022-12-31"),
+    )
+    assert msg == "Incorrect value for patient 1: expected 49, got 50 instead."
+
+
+@pytest.mark.parametrize(
+    "order, message",
+    [
+        ([1, 0], "Missing row(s): 1, 3, 6, 9, 10, 13, 15, 17, 19, 20."),
+        ([0, 1], "Found extra row(s): 1, 3, 6, 9, 10, 13, 15, 17, 19, 20."),
+    ],
+)
+def test_event_table_has_missing_or_extra_rows(engine, order, message):
+    tables = [
+        clinical_events,
+        clinical_events.where(clinical_events.snomedct_code.is_in(["60621009"])),
+    ]
+    answer, expected = (tables[i] for i in order)
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    assert msg == message
+
+
+@pytest.mark.parametrize(
+    "order, message",
+    [
+        ([1, 0], "Missing row(s): 5, 9."),
+        ([0, 1], "Found extra row(s): 5, 9."),
+    ],
+)
+def test_event_series_has_missing_or_extra_rows(engine, order, message):
+    series = [
+        medications.dmd_code,
+        medications.where(medications.date.is_on_or_before("2020-12-01")).dmd_code,
+    ]
+    answer, expected = (series[i] for i in order)
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    assert msg == message
+
+
+def test_event_series_has_incorrect_value(engine):
+    answer = medications.date
+    expected = medications.dmd_code
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    assert (
+        msg
+        == "Incorrect value for patient 1, row 1: expected 39113611000001102, got 2014-01-11 instead."
+    )
+
+
+def test_incorrect_event_selection_for_patient(engine):
+    events = clinical_events.where(
+        clinical_events.snomedct_code.is_in(["60621009"])
+    ).sort_by(clinical_events.date)
+    answer = events.first_for_patient()
+    expected = events.last_for_patient()
+    msg = quiz.check_answer(engine=engine, answer=answer, expected=expected)
+    # TODO: This might not be the most helpful error message.
+    assert (
+        msg
+        == "Incorrect `numeric_value` value for patient 2: expected 23.1, got 18.4 instead."
+    )
