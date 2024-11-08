c0f81511079c3870e72cdd612fd02f9feeb80faf
Becky Smith <becky.smith@thedatalab.org>
2024-11-08 15:20:56 +0000

    Optional label for debug line

--- ehrql/debug.py
+++ ehrql/debug.py
@@ -2,14 +2,14 @@ import inspect
 import sys
 
 
-def show(element):
+def show(element, label=""):
     """
     Show the output of the specified element within a
     dataset definition (a string, value, dataset variable, table
     etc.)
     """
     line_no = inspect.getframeinfo(sys._getframe(1))[1]
-    print(f"Debug at line {line_no}:", file=sys.stderr)
+    print(f"Debug line {line_no}: {label}", file=sys.stderr)
     print(element, file=sys.stderr)
 
 
