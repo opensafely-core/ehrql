84859e2e64e9adb5158c8ad0b77c048f2499cba8
Becky Smith <becky.smith@thedatalab.org>
2024-11-13 17:39:10 +0000

    Options to show head/tail of column/table/dataset debug output
    In addition to head/tail kwargs to the show() function, stop() also
    takes head/tail kwargs, which can be used to truncate the final
    displayed dataset as well, by putting it at the end of the dataset
    definition.

--- ehrql/debug.py
+++ ehrql/debug.py
@@ -1,22 +1,56 @@
 import inspect
 import sys
 
+from ehrql.renderers import truncate_table
 
-def show(element, label=""):
+
+def show(
+    element, label: str | None = None, head: int | None = None, tail: int | None = None
+):
     """
-    Show the output of the specified element within a
-    dataset definition (a string, value, dataset variable, table
-    etc.)
+    Show the output of the specified element within a dataset definition
+
+    _element_<br>
+    Any element within the dataset definition file; can be a string, constant value etc,
+    but will typically be a dataset variable (filtered table, column, or a dataset itself.)
+
+    _label_<br>
+    Optional label which will be printed in the debug output.
+
+    _head_<br>
+    Show only the first N lines. If the output is an ehrQL column, table or dataset, it will
+    print only the first N lines of the table.
+
+    _tail_<br>
+    Show only the last N lines. If the output is an ehrQL column, table or dataset, it will
+    print only the last N lines of the table.
+
+    head and tail arguments can be combined, e.g. to show the first and last 5 lines of a table:
+
+      show(<table>, head=5, tail=5)
     """
     line_no = inspect.getframeinfo(sys._getframe(1))[1]
-    print(f"Debug line {line_no}: {label}", file=sys.stderr)
-    print(element, file=sys.stderr)
+    element_repr = repr(element)
+    if head or tail:
+        element_repr = truncate_table(element_repr, head, tail)
+    label = f" {label}" if label else ""
+    print(f"Debug line {line_no}:{label}", file=sys.stderr)
+    print(element_repr, file=sys.stderr)
 
 
-def stop():
+def stop(*, head: int | None = None, tail: int | None = None):
     """
-    Stop loading the dataset definition at this point and show the
-    current contents of the dataset.
+    Stop loading the dataset definition and show the contents of the dataset at this point.
+
+    _head_<br>
+    Show only the first N lines of the dataset.
+
+    _tail_<br>
+    Show only the last N lines of the dataset.
+
+    head and tail arguments can be combined, e.g. to show the first and last 5 lines of the dataset:
+
+      stop(head=5, tail=5)
     """
     line_no = inspect.getframeinfo(sys._getframe(1))[1]
     print(f"Stopping at line {line_no}", file=sys.stderr)
--- ehrql/renderers.py
+++ ehrql/renderers.py
@@ -1,4 +1,7 @@
-def records_to_html_table(records):
+import re
+
+
+def records_to_html_table(records: list[dict]):
     rows = []
     headers_written = False
 
@@ -8,12 +11,12 @@ def records_to_html_table(records):
             headers_written = True
         row = "".join(f"<td>{val}</td>" for val in record.values())
         rows.append(f"<tr>{row}</tr>")
-    rows = "".join([f"<tr>{row}</tr>" for row in rows])
+    rows = "".join(rows)
 
     return f"<table><thead>{headers}</thead><tbody>{rows}</tbody></table>"
 
 
-def records_to_ascii_table(records):
+def records_to_ascii_table(records: list[dict]):
     width = 17
     lines = []
     headers_written = False
@@ -24,6 +27,104 @@ def records_to_ascii_table(records):
             headers_written = True
         lines.append(" | ".join(str(value).ljust(width) for value in record.values()))
     return "\n".join(line.strip() for line in lines) + "\n"
+
+
+def _truncate_html_table(table_repr: str, head: int | None, tail: int | None):
+    """
+    Truncate an html table string to the first/last N rows, with a row of ...
+    values to indicate where it's been truncated
+    """
+    regex = re.compile(
+        r"(?P<start>^<table>.*<tbody>)(?P<rows><tr>.*<\/tr>)(?P<end><\/tbody>.*<\/table>)"
+    )
+    match = regex.match(table_repr)
+    if match is None:
+        # if we can't parse the table, return None and let the fallback handle it
+        return
+
+    start = match.group("start")
+    rows = match.group("rows")
+    end = match.group("end")
+
+    # Check we have enough rows to truncate to head and tail
+    if rows.count("<tr>") <= ((head or 0) + (tail or 0)):
+        return table_repr
+
+    # split row on <tr> tokens, remove any empty strings (this will lose the first <tr> of
+    # each row, but we'll add it in again later)
+    rows = [row for row in rows.split("<tr>") if row]
+    # compose an "ellipsis row" to mark the place of truncated rows
+    td_count = rows[0].count("<td>")
+    ellipsis_row = f"{'<td>...</td>'*td_count}</tr>"
+
+    # Build the list of rows we need to include, with ellipsis rows where necessary
+    truncated_rows = []
+
+    head_rows = rows[:head] if head is not None else [ellipsis_row]
+    truncated_rows.extend(head_rows)
+
+    if head is not None and tail is not None:
+        truncated_rows.append(ellipsis_row)
+
+    tail_rows = rows[-tail:] if tail is not None else [ellipsis_row]
+    truncated_rows.extend(tail_rows)
+
+    # re-join the truncated rows with <tr>
+    truncated_rows = "<tr>" + "<tr>".join(truncated_rows)
+    return start + truncated_rows + end
+
+
+def _truncate_lines(
+    table_repr: str, headers: int = 0, head: int | None = None, tail: int | None = None
+):
+    table_rows = [row for row in table_repr.split("\n") if row]
+
+    # Check we have enough rows to truncate to head and tail
+    if len(table_rows) <= (headers + (head or 0) + (tail or 0)):
+        return table_repr
+
+    # compose an "ellipsis row" to mark the place of truncated rows
+    cell_count = table_rows[0].count("|") + 1
+    ellipsis_row = " | ".join("...".ljust(17) for i in range(cell_count)).strip()
+
+    # Build the list of rows we need to include, with ellipsis rows where necessary
+    truncated_rows = table_rows[:headers]
+
+    head_rows = (
+        table_rows[headers : headers + head] if head is not None else [ellipsis_row]
+    )
+    truncated_rows.extend(head_rows)
+
+    if head is not None and tail is not None:
+        truncated_rows.append(ellipsis_row)
+
+    tail_rows = table_rows[-tail:] if tail is not None else [ellipsis_row]
+    truncated_rows.extend(tail_rows)
+
+    return "\n".join(truncated_rows)
+
+
+def truncate_table(table_repr: str, head: int | None, tail: int | None):
+    """
+    Take a table repr (ascii or html format) and truncate it to show only the
+    first and/or last N rows.
+    """
+    if head is None and tail is None:
+        return table_repr
+
+    truncated_repr = None
+
+    if "<table>" in table_repr:
+        truncated_repr = _truncate_html_table(table_repr, head=head, tail=tail)
+    elif "---+---" in table_repr:
+        truncated_repr = _truncate_lines(table_repr, headers=2, head=head, tail=tail)
+
+    # if we didn't detect either an ascii or html table, or the html regex
+    # didn't match as expected, fall back to a simple truncation of lines using
+    # line breaks.
+    if truncated_repr is None:
+        truncated_repr = _truncate_lines(table_repr, head=head, tail=tail)
+    return truncated_repr
 
 
 DISPLAY_RENDERERS = {
