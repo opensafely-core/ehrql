f9643221f8f41051a6524129b112e7935db6fdf9
Peter Inglesby <peter.inglesby@gmail.com>
2024-11-20 18:36:00 +0000

    Reuse activate_debug_context for rendering in sandbox
    Tests are simplified, because the rendering behaviour is covered
    elsewhere.

--- tests/unit/test_sandbox.py
+++ tests/unit/test_sandbox.py
@@ -7,45 +7,15 @@ from ehrql.sandbox import run
 
 
 user_input = """
-from ehrql import Dataset
+from ehrql import create_dataset
 from ehrql.tables.core import patients
-patients.date_of_birth
-patients
-dataset = Dataset()
+dataset = create_dataset()
 dataset.sex = patients.sex
-dataset
 dataset.define_population(patients.date_of_birth > "2000-01-01")
 dataset
 """
 
-expected_series_output = """
-patient_id        | value
-------------------+------------------
-1                 | 1980-01-01
-2                 | 1990-02-01
-3                 | 2000-03-01
-4                 | 2010-04-01
-""".strip()
-
-expected_frame_output = """
-patient_id        | date_of_birth     | sex               | date_of_death
-------------------+-------------------+-------------------+------------------
-1                 | 1980-01-01        | female            | None
-2                 | 1990-02-01        | male              | None
-3                 | 2000-03-01        | female            | None
-4                 | 2010-04-01        | male              | None
-""".strip()
-
-expected_dataset_output_1 = """
-patient_id        | sex
-------------------+------------------
-1                 | female
-2                 | male
-3                 | female
-4                 | male
-""".strip()
-
-expected_dataset_output_2 = """
+expected_dataset_output = """
 patient_id        | sex
 ------------------+------------------
 3                 | female
@@ -58,18 +28,7 @@ def test_run(capsys, monkeypatch):
     dummy_tables_path = Path(__file__).parents[1] / "fixtures" / "sandbox"
     run(dummy_tables_path)
     captured = capsys.readouterr()
-    assert expected_series_output in captured.out, captured.out
-    assert expected_frame_output in captured.out
-    assert expected_dataset_output_1 in captured.out
-    assert expected_dataset_output_2 in captured.out
-
-
-def test_run_with_empty_dataset(capsys, monkeypatch):
-    monkeypatch.setattr("sys.stdin", StringIO("from ehrql import Dataset; Dataset()"))
-    dummy_tables_path = Path(__file__).parents[1] / "fixtures" / "sandbox"
-    run(dummy_tables_path)
-    captured = capsys.readouterr()
-    assert captured.out == ">>> Dataset()\n>>> "
+    assert expected_dataset_output in captured.out
 
 
 def test_traceback_trimmed(capsys, monkeypatch):
