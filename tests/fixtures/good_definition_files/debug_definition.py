bac3140133784e873e1bd3c4be923d82ffa7be2f
Richard Williams <richard.williams@thedatalab.org>
2024-12-13 10:44:12 +0000

    `show()` no longer accepts strings/ints etc and throws, so rejig some other tests

--- tests/fixtures/good_definition_files/debug_definition.py
+++ tests/fixtures/good_definition_files/debug_definition.py
@@ -4,7 +4,7 @@ from ehrql.tables.core import patients
 
 
 dataset = create_dataset()
+show(dataset)
 dataset.sex = patients.sex
-show("Hello")
 dataset.define_population(patients.date_of_birth.is_on_or_after("2000-01-01"))
 dataset.year_of_birth = patients.date_of_birth.year
