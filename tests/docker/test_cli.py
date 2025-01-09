f78f6edf578954159a464ba8a9c43adbe2f7226e
David Evans <dave.evans@thedatalab.org>
2025-01-09 13:08:15 +0000

    Update Docker CLI tests to use new mechanism

--- tests/docker/test_cli.py
+++ tests/docker/test_cli.py
@@ -3,42 +3,56 @@ from datetime import datetime
 import pytest
 
 from tests.lib.docker import ContainerError
+from tests.lib.file_utils import read_file_as_dicts
 from tests.lib.inspect_utils import function_body_as_string
 from tests.lib.tpp_schema import AllowedPatientsWithTypeOneDissent, Patient
 
 
-@function_body_as_string
-def trivial_dataset_definition():
-    from ehrql import create_dataset
-    from ehrql.tables.tpp import patients
-
-    dataset = create_dataset()
-    year = patients.date_of_birth.year
-    dataset.define_population(year >= 1940)
-    dataset.year = year
-
-    dataset.configure_dummy_data(
-        population_size=10,
-        additional_population_constraint=patients.date_of_death.is_null(),
-    )
-
-
-def test_generate_dataset_in_container(study, mssql_database):
+def test_generate_dataset_in_container(tmp_path, call_cli_docker, mssql_database):
     mssql_database.setup(
         Patient(Patient_ID=1, DateOfBirth=datetime(1943, 5, 5)),
         AllowedPatientsWithTypeOneDissent(Patient_ID=1),
     )
 
-    study.setup_from_string(trivial_dataset_definition)
-    study.generate_in_docker(mssql_database, "ehrql.backends.tpp.TPPBackend")
-    results = study.results()
+    @function_body_as_string
+    def dataset_definition():
+        from ehrql import create_dataset
+        from ehrql.tables.tpp import patients
+
+        dataset = create_dataset()
+        year = patients.date_of_birth.year
+        dataset.define_population(year >= 1940)
+        dataset.year = year
+
+        dataset.configure_dummy_data(
+            population_size=10,
+            additional_population_constraint=patients.date_of_death.is_null(),
+        )
+
+    dataset_definition_path = tmp_path / "dataset_definition.py"
+    dataset_definition_path.write_text(dataset_definition)
+
+    output_path = tmp_path / "results.csv"
+
+    call_cli_docker(
+        "generate-dataset",
+        dataset_definition_path,
+        "--output",
+        output_path,
+        workspace=tmp_path,
+        environ={
+            "OPENSAFELY_BACKEND": "tpp",
+            "DATABASE_URL": mssql_database.container_url(),
+        },
+    )
+    results = read_file_as_dicts(output_path)
 
     assert len(results) == 1
     assert results[0]["year"] == "1943"
 
 
 def test_generate_dataset_with_disallowed_operations_in_container(
-    study, mssql_database
+    tmp_path, call_cli_docker, mssql_database
 ):
     # End-to-end test to confirm that disallowed operations are blocked when running
     # inside the Docker container. Obviously the below is not a valid dataset definition
@@ -55,8 +69,18 @@ def test_generate_dataset_with_disallowed_operations_in_container(
         except TimeoutError:
             pass
 
-    study.setup_from_string(dataset_definition)
+    dataset_definition_path = tmp_path / "dataset_definition.py"
+    dataset_definition_path.write_text(dataset_definition)
+
     with pytest.raises(
         ContainerError, match=r"PermissionError: \[Errno 1\] Operation not permitted"
     ):
-        study.generate_in_docker(mssql_database, "tpp")
+        call_cli_docker(
+            "generate-dataset",
+            dataset_definition_path,
+            workspace=tmp_path,
+            environ={
+                "OPENSAFELY_BACKEND": "tpp",
+                "DATABASE_URL": mssql_database.container_url(),
+            },
+        )
