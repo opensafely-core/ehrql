89493ae341917ab06feca4d79df41344e49e31bb
David Evans <dave.evans@thedatalab.org>
2025-01-15 16:15:49 +0000

    Add test for `iter_flatten`
    This was implicitly exercised elsewhere but it's nice to have an
    explicit test for it in the unit tests.

--- tests/unit/utils/test_itertools_utils.py
+++ tests/unit/utils/test_itertools_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ehrql.utils.itertools_utils import eager_iterator
+from ehrql.utils.itertools_utils import eager_iterator, iter_flatten
 
 
 def test_eager_iterator():
@@ -37,3 +37,15 @@ def test_eager_iterator_still_mostly_lazy():
 def test_eager_iterator_works_on_lists():
     results = eager_iterator([1, 2, 3])
     assert list(results) == [1, 2, 3]
+
+
+def test_iter_flatten():
+    nested = [
+        1,
+        2,
+        (3, (4, [5]), [6, 7]),
+        [8, (i for i in range(9, 11))],
+        "foo",
+    ]
+    flattened = list(iter_flatten(nested))
+    assert flattened == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "foo"]
