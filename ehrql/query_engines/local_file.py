opensafely-core/cohort-extractor-v2

Update local_file query engine function signature

ehrql/query_engines/local_file.py
@@ -14,14 +14,14 @@ class LocalFileQueryEngine(InMemoryQueryEngine):
 
     database = None
 
-    def get_results_stream(self, dataset):
+    def get_results_stream(self, dataset, measures=None):
         # Given the dataset supplied determine the tables used and load the associated
         # data into the database
         self.populate_database(
             get_table_nodes(dataset),
         )
         # Run the query as normal
-        return super().get_results_stream(dataset)
+        return super().get_results_stream(dataset, measures)
 
     def populate_database(self, table_nodes, allow_missing_columns=True):
         table_specs = {

opensafely-core/ehrql

ehrql/query_engines/local_file.py
@@ -14,14 +14,14 @@ class LocalFileQueryEngine(InMemoryQueryEngine):
 
     database = None
 
-    def get_results_tables(self, dataset):
+    def get_results_tables(self, dataset, measures=None):
         # Given the dataset supplied determine the tables used and load the associated
         # data into the database
         self.populate_database(
             get_table_nodes(dataset),
         )
         # Run the query as normal
-        return super().get_results_tables(dataset)
+        return super().get_results_tables(dataset, measures=measures)
 
     def populate_database(self, table_nodes, allow_missing_columns=True):
         table_specs = {
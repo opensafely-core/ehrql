8b9655b0d1f5237bdd153d1f0235b4c891ff1c8a
Becky Smith <becky.smith@thedatalab.org>
2025-01-29 10:45:09 +0000

    Update local_file query engine function signature

--- ehrql/query_engines/local_file.py
+++ ehrql/query_engines/local_file.py
@@ -14,14 +14,14 @@ class LocalFileQueryEngine(InMemoryQueryEngine):
 
     database = None
 
-    def get_results_tables(self, dataset):
+    def get_results_tables(self, dataset, measures=None):
         # Given the dataset supplied determine the tables used and load the associated
         # data into the database
         self.populate_database(
             get_table_nodes(dataset),
         )
         # Run the query as normal
-        return super().get_results_tables(dataset)
+        return super().get_results_tables(dataset, measures=measures)
 
     def populate_database(self, table_nodes, allow_missing_columns=True):
         table_specs = {
