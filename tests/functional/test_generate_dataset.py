87b54fe0af03d677c063e506064bf1bb02dc95a9
David Evans <dave.evans@thedatalab.org>
2025-09-19 16:41:59 +0100

    Add functional tests for permissions claims

--- tests/functional/test_generate_dataset.py
+++ tests/functional/test_generate_dataset.py
@@ -641,3 +641,51 @@ def test_generate_dataset_allows_sufficient_permissions(
     )
 
     assert output_path.exists()
+
+
+def test_generate_dataset_warns_on_missing_permissions_for_dummy_data(
+    call_cli, tmp_path, caplog
+):
+    dataset_definition_path = tmp_path / "dataset_definition.py"
+    dataset_definition_path.write_text(dataset_definition_with_restricted_table)
+    output_path = tmp_path / "results.csv"
+
+    call_cli(
+        "generate-dataset",
+        dataset_definition_path,
+        "--output",
+        output_path,
+    )
+
+    assert output_path.exists()
+
+    output = caplog.text
+    assert "restricted_table" in output
+    assert 'claim_permissions("special_perm")' in output
+
+
+def test_generate_dataset_does_not_warn_when_permission_claimed(
+    call_cli, tmp_path, caplog
+):
+    dataset_definition_with_claim = (
+        f"from ehrql import claim_permissions\n"
+        f"claim_permissions('special_perm')\n"
+        f"\n"
+        f"{dataset_definition_with_restricted_table}"
+    )
+    dataset_definition_path = tmp_path / "dataset_definition.py"
+    dataset_definition_path.write_text(dataset_definition_with_claim)
+    output_path = tmp_path / "results.csv"
+
+    call_cli(
+        "generate-dataset",
+        dataset_definition_path,
+        "--output",
+        output_path,
+    )
+
+    assert output_path.exists()
+
+    output = caplog.text
+    assert "restricted_table" not in output
+    assert 'claim_permissions("special_perm")' not in output
