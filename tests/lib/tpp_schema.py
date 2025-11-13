0c6e0ac75803372c637bbdaeb0f7d070760b930a
Becky Smith <becky.smith@thedatalab.org>
2025-11-13 14:59:47 +0000

    Run just update-tpp-schema to update tpp_schema.py

--- tests/lib/tpp_schema.py
+++ tests/lib/tpp_schema.py
@@ -6708,6 +6708,7 @@ class Organisation(Base):
     __tablename__ = "Organisation"
     _pk = mapped_column(t.Integer, primary_key=True)
 
+    DirectionsAcknowledged = mapped_column(t.Boolean)
     GoLiveDate = mapped_column(
         t.DateTime, nullable=False, default="9999-12-31T00:00:00"
     )
