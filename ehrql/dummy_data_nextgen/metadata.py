ed738b3ef94bd3f010a32677551095ef87870c4c
Alice Wong <alice.wong@thedatalab.org>
2026-01-08 15:55:01 +0000

    Add date_A <= date_B metadata for date columns
    Have one unit test run per pair of date columns we want to specify
    constraints for.
    
    We see that it is currently not possible to specify two
    `RelatedToOther` constraints on the same column, so two tests are
    marked with `xfail`.
    
    Also note that waiting times data (`wl_clockstops` and
    `wl_openpathways`) require more complicated logic and probably
    specific tests, so for now, we only add the simplest constraint to
    specify.
    
    (Note: We do not specify a constraint for
    `practice_registrations`'s `start_date` and `end_date` columns since the
    specialised generator for that table already ensures that `start_date`
    is the date of birth and the end date is `None`.)

--- ehrql/dummy_data_nextgen/metadata.py
+++ ehrql/dummy_data_nextgen/metadata.py
@@ -45,4 +45,68 @@ def get_dummy_data_constraints(table_name, column_name):
             "start_date": [DummyDataConstraint.RelatedToOther("end_date", "<=")],
             "end_date": [DummyDataConstraint.RelatedToOther("start_date", ">=")],
         },
+        "apcs": {
+            "admission_date": [
+                DummyDataConstraint.RelatedToOther("discharge_date", "<=")
+            ],
+            "discharge_date": [
+                DummyDataConstraint.RelatedToOther("admission_date", ">=")
+            ],
+        },
+        "apcs_cost": {
+            "admission_date": [
+                DummyDataConstraint.RelatedToOther("discharge_date", "<=")
+            ],
+            "discharge_date": [
+                DummyDataConstraint.RelatedToOther("admission_date", ">=")
+            ],
+        },
+        "appointments": {
+            "booked_date": [DummyDataConstraint.RelatedToOther("start_date", "<=")],
+            "start_date": [DummyDataConstraint.RelatedToOther("booked_date", ">=")],
+        },
+        "ec_cost": {
+            "arrival_date": [
+                DummyDataConstraint.RelatedToOther("ec_decision_to_admit_date", "<=")
+            ],
+            "ec_decision_to_admit_date": [
+                DummyDataConstraint.RelatedToOther("arrival_date", ">=")
+            ],
+        },
+        **{
+            op_table: {
+                "referral_request_received_date": [
+                    DummyDataConstraint.RelatedToOther("appointment_date", "<=")
+                ],
+                "appointment_date": [
+                    DummyDataConstraint.RelatedToOther(
+                        "referral_request_received_date", ">="
+                    )
+                ],
+            }
+            for op_table in ["opa", "opa_cost", "opa_diag", "opa_proc"]
+        },
+        "sgss_covid_all_tests": {
+            "specimen_taken_date": [
+                DummyDataConstraint.RelatedToOther("lab_report_date", "<=")
+            ],
+            "lab_report_date": [
+                DummyDataConstraint.RelatedToOther("specimen_taken_date", ">=")
+            ],
+        },
+        **{
+            wl_table: {
+                "referral_to_treatment_period_start_date": [
+                    DummyDataConstraint.RelatedToOther(
+                        "referral_to_treatment_period_end_date", "<="
+                    )
+                ],
+                "referral_to_treatment_period_end_date": [
+                    DummyDataConstraint.RelatedToOther(
+                        "referral_to_treatment_period_start_date", ">="
+                    )
+                ],
+            }
+            for wl_table in ["wl_clockstops", "wl_openpathways"]
+        },
     }.get(table_name, {}).get(column_name, [])
--- tests/unit/dummy_data_nextgen/test_metadata.py
+++ tests/unit/dummy_data_nextgen/test_metadata.py
@@ -6,6 +6,17 @@ from ehrql.dummy_data_nextgen.metadata import DummyDataConstraint
 from ehrql.tables.core import practice_registrations
 from ehrql.tables.tpp import (
     addresses,
+    apcs,
+    apcs_cost,
+    appointments,
+    ec_cost,
+    opa,
+    opa_cost,
+    opa_diag,
+    opa_proc,
+    sgss_covid_all_tests,
+    wl_clockstops,
+    wl_openpathways,
 )
 
 
@@ -94,6 +105,58 @@ def test_dummy_data_generator_only_generates_positive_practice_pseudo_id_up_to_9
     "table,earlier_date_col_name,later_date_col_name",
     [
         (addresses, "start_date", "end_date"),
+        (apcs, "admission_date", "discharge_date"),
+        (apcs_cost, "admission_date", "discharge_date"),
+        (appointments, "booked_date", "start_date"),
+        pytest.param(
+            appointments,
+            "start_date",
+            "seen_date",
+            marks=pytest.mark.xfail(
+                reason="double specification for start_date not supported", strict=True
+            ),
+        ),
+        (ec_cost, "arrival_date", "ec_decision_to_admit_date"),
+        pytest.param(
+            ec_cost,
+            "ec_injury_date",
+            "arrival_date",
+            marks=pytest.mark.xfail(
+                reason="double specification for arrival_date not supported",
+                strict=True,
+            ),
+        ),
+        (
+            opa,
+            "referral_request_received_date",
+            "appointment_date",
+        ),
+        (
+            opa_cost,
+            "referral_request_received_date",
+            "appointment_date",
+        ),
+        (
+            opa_diag,
+            "referral_request_received_date",
+            "appointment_date",
+        ),
+        (
+            opa_proc,
+            "referral_request_received_date",
+            "appointment_date",
+        ),
+        (sgss_covid_all_tests, "specimen_taken_date", "lab_report_date"),
+        (
+            wl_clockstops,
+            "referral_to_treatment_period_start_date",
+            "referral_to_treatment_period_end_date",
+        ),
+        (
+            wl_openpathways,
+            "referral_to_treatment_period_start_date",
+            "referral_to_treatment_period_end_date",
+        ),
     ],
 )
 def test_dummy_data_generator_with_one_date_constrained_to_be_before_another(
