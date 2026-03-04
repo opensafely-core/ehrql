c9f57a4d68423f46864218040426fdc21658843f
Dave Evans <dave.evans@thedatalab.org>
2026-03-04 11:00:53 +0000

    Update warning text to match OpenSAFELY docs
    See:
     * https://github.com/opensafely/documentation/pull/1922

--- ehrql/backends/emis.py
+++ ehrql/backends/emis.py
@@ -12,10 +12,13 @@ from ehrql.query_engines.trino import TrinoQueryEngine
 class EMISBackend(SQLBackend):
     """
     !!! warning
-        Research access to the backend provided by EMIS is temporarily unavailable,
-        pending funding arrangements between NHS England and EMIS.
-        When funding has been secured,
-        we will publish a timeline for gradually reopening access.
+        Research access to the backend provided by EMIS is temporarily paused, pending
+        completion of contracting between NHS England and Optum/EMIS. Examples of whole
+        population work delivered using this service include research on [coding of long
+        COVID](https://pubmed.ncbi.nlm.nih.gov/34340970/), and on changes in [primary
+        care activity](https://pubmed.ncbi.nlm.nih.gov/37498081/) and [medication
+        safety](https://pubmed.ncbi.nlm.nih.gov/37303488/) during the pandemic. We will
+        update on the news pages when NHS England have completed their contract.
 
     [EMIS Health](https://www.emishealth.com/) are the developers and operators of the
     [EMIS Web](https://www.emishealth.com/products/emis-web) EHR platform. The ehrQL
