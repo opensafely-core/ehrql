f812c5ada13b20b00a330c70234aaa6730c62971
Alice Wong <alice.wong@thedatalab.org>
2026-04-01 16:03:12 +0100

    Change EMIS v2 test schema `Patient` column types
    Connecting to the EMIS staging environment via
    SQLAlchemy revealed that:
    
    - `patient_id` is defined as `VARBINARY` without a specified length.
    Previously 16 was assumed based on the example value provided by Optum,
    but it seems safer not to assume a specific length.
    
    - `date_of_birth` uses the Trino `TIMESTAMP` type rather than
    SQLAlchemy's `DateTime`.
    
    Use the types obtained from the staging environment instead.
    
    I chose the `trdt` shorthand for `trino.sqlalchemy.datatype` somewhat
    arbitrarily, but it seems distinct enough from `t` and not too long to
    be cluttering in the file.
    
    Note that I was unable to obtain primary key / foreign key information
    from connecting to the staging environment. I have nonetheless kept
    the `unique` constraint on `patient_id` since the schema spreadsheet
    provided by Optum indicates that this is a unique identifier.

--- tests/backend_schemas/emisv2/schema.py
+++ tests/backend_schemas/emisv2/schema.py
@@ -1,5 +1,6 @@
 from sqlalchemy import types as t
 from sqlalchemy.orm import DeclarativeBase, mapped_column
+from trino.sqlalchemy import datatype as trdt
 
 
 class Base(DeclarativeBase):
@@ -10,5 +11,5 @@ class Patient(Base):
     __tablename__ = "patient"
     _pk = mapped_column(t.Integer, primary_key=True)
 
-    patient_id = mapped_column(t.VARBINARY(16), unique=True)
-    date_of_birth = mapped_column(t.DateTime)
+    patient_id = mapped_column(t.VARBINARY(), unique=True)
+    date_of_birth = mapped_column(trdt.TIMESTAMP(precision=6))
