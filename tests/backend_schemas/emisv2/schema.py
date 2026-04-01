88090a2ad821b76fed386c5c5dc60cf1e805b303
Alice Wong <alice.wong@thedatalab.org>
2026-04-01 16:37:57 +0100

    Add EMISv2 observation and medication issue schema
    - Types were obtained from connecting to the EMIS staging environment
    via SQLAlchemy.
    
    - Test values used were adapted from the excel sheet provided by Optum,
    except for the binary patient_id which is `bytes(range(16))` for
    clarity.
    
    - Tables are ordered alphabetically to be consistent with the TPP
    schema file.
    
    - This commit does not include the `consultation_id` column in the two
    tables; the next commit will do so.

--- tests/backend_schemas/emisv2/schema.py
+++ tests/backend_schemas/emisv2/schema.py
@@ -7,9 +7,28 @@ class Base(DeclarativeBase):
     "Common base class to signal that models below belong to the same database"
 
 
+class MedicationIssueRecord(Base):
+    __tablename__ = "medication_issue_record"
+    _pk = mapped_column(t.Integer, primary_key=True)
+
+    patient_id = mapped_column(t.VARBINARY())
+    dmd_product_code_id = mapped_column(t.BIGINT())
+    effective_datetime = mapped_column(trdt.TIMESTAMP(precision=6, timezone=True))
+
+
 class Patient(Base):
     __tablename__ = "patient"
     _pk = mapped_column(t.Integer, primary_key=True)
 
     patient_id = mapped_column(t.VARBINARY(), unique=True)
     date_of_birth = mapped_column(trdt.TIMESTAMP(precision=6))
+
+
+class Observation(Base):
+    __tablename__ = "observation"
+    _pk = mapped_column(t.Integer, primary_key=True)
+
+    patient_id = mapped_column(t.VARBINARY())
+    effective_datetime = mapped_column(trdt.TIMESTAMP(precision=6, timezone=True))
+    numeric_value = mapped_column(t.DECIMAL(precision=19, scale=3))
+    snomed_concept_id = mapped_column(t.BIGINT())
--- tests/unit/backend_schemas/test_emisv2.py
+++ tests/unit/backend_schemas/test_emisv2.py
@@ -15,19 +15,59 @@ def test_schema():
     engine = sqlalchemy.create_engine("sqlite+pysqlite:///:memory:")
     schema.Base.metadata.create_all(engine)
 
+    new_medication_issue_record = schema.MedicationIssueRecord(
+        patient_id=bytes(range(16)),
+        dmd_product_code_id=12354611500001104,
+        effective_datetime=datetime.datetime(
+            2023, 5, 12, 14, 30, 15, 0, tzinfo=datetime.UTC
+        ),
+    )
     new_patient = schema.Patient(
         patient_id=bytes(range(16)),
         date_of_birth=datetime.datetime(2023, 5, 1, 0, 0, 0, 0),
     )
+    new_observation = schema.Observation(
+        patient_id=bytes(range(16)),
+        effective_datetime=datetime.datetime(
+            2023, 5, 12, 14, 30, 15, 0, tzinfo=datetime.UTC
+        ),
+        numeric_value=80.0,
+        snomed_concept_id=123456789,
+    )
     with sqlalchemy.orm.Session(engine) as session:
+        session.add(new_medication_issue_record)
         session.add(new_patient)
+        session.add(new_observation)
         session.commit()
+        selected_medication_issue_record = session.execute(
+            sqlalchemy.select(schema.MedicationIssueRecord)
+        ).scalar_one()
         selected_patient = session.execute(
             sqlalchemy.select(schema.Patient)
         ).scalar_one()
+        selected_observation = session.execute(
+            sqlalchemy.select(schema.Observation)
+        ).scalar_one()
 
+    # SQLite does not support timezone-aware datetimes
+    # so in these assertions we have to ignore the timezone
     assert selected_patient._pk == 1
     assert selected_patient.patient_id == bytes(range(16))
     assert selected_patient.date_of_birth == datetime.datetime(2023, 5, 1, 0, 0, 0, 0)
+
+    assert selected_medication_issue_record._pk == 1
+    assert selected_medication_issue_record.patient_id == bytes(range(16))
+    assert selected_medication_issue_record.dmd_product_code_id == 12354611500001104
+    assert selected_medication_issue_record.effective_datetime == datetime.datetime(
+        2023, 5, 12, 14, 30, 15, 0
+    )
+
+    assert selected_observation._pk == 1
+    assert selected_observation.patient_id == bytes(range(16))
+    assert selected_observation.effective_datetime == datetime.datetime(
+        2023, 5, 12, 14, 30, 15, 0
+    )
+    assert selected_observation.numeric_value == 80.0
+    assert selected_observation.snomed_concept_id == 123456789
 
     engine.dispose()
