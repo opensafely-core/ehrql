53188d22a77a9b5c6db304970120c05162475ea4
Alice Wong <alice.wong@thedatalab.org>
2026-04-01 16:02:42 +0100

    Refactor temporary test for EMIS v2 test schema
    We will be asserting more definitions within this test,
    so rename it `test_schema` and lightly refactor the imports
    and variable names.

--- tests/unit/backend_schemas/test_emisv2.py
+++ tests/unit/backend_schemas/test_emisv2.py
@@ -3,29 +3,31 @@ import datetime
 import sqlalchemy
 import sqlalchemy.orm
 
-from tests.backend_schemas.emisv2.schema import Base, Patient
+from tests.backend_schemas.emisv2 import schema
 
 
-def test_patient():
+def test_schema():
     """
     Validate that the patient table is correctly defined.
     This is a temporary test since we do not have a EMIS v2 backend yet;
     when we have one, this test can be removed.
     """
     engine = sqlalchemy.create_engine("sqlite+pysqlite:///:memory:")
-    Base.metadata.create_all(engine)
+    schema.Base.metadata.create_all(engine)
 
-    new_patient = Patient(
+    new_patient = schema.Patient(
         patient_id=bytes(range(16)),
         date_of_birth=datetime.datetime(2023, 5, 1, 0, 0, 0, 0),
     )
     with sqlalchemy.orm.Session(engine) as session:
         session.add(new_patient)
         session.commit()
-        result = session.execute(sqlalchemy.select(Patient)).scalar_one()
+        selected_patient = session.execute(
+            sqlalchemy.select(schema.Patient)
+        ).scalar_one()
 
-    assert result._pk == 1
-    assert result.patient_id == bytes(range(16))
-    assert result.date_of_birth == datetime.datetime(2023, 5, 1, 0, 0, 0, 0)
+    assert selected_patient._pk == 1
+    assert selected_patient.patient_id == bytes(range(16))
+    assert selected_patient.date_of_birth == datetime.datetime(2023, 5, 1, 0, 0, 0, 0)
 
     engine.dispose()
