e3a41b648d11f33306ef37a71bb298fbcbc88b24
Dave Evans <dave.evans@thedatalab.org>
2026-04-10 15:53:06 +0100

    Create the temporary tables schema which the backend now expects

--- tests/support/trino/setup.sql
+++ tests/support/trino/setup.sql
@@ -1,1 +1,4 @@
--- SQL to setup the database goes here
+-- In production each user has a writable schema named with their username in which to
+-- create temporary tables; we want to mirror that set up here so we create a "trino"
+-- schema (named after the default user") in the default "trino" catalog.
+CREATE SCHEMA trino.trino;
