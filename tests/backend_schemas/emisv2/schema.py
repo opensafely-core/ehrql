fd47279eda233daba1f3f6b4031371ae0d5e3c82
Alice Wong <alice.wong@thedatalab.org>
2026-04-27 15:11:14 +0100

    Do not instantiate column types if unnecessary
    If no keyword arguments are passed,
    there is no need to instantiate the types.
    This matches tpp/schema.py.

--- tests/backend_schemas/emisv2/schema.py
+++ tests/backend_schemas/emisv2/schema.py
@@ -11,8 +11,8 @@ class MedicationIssueRecord(Base):
     __tablename__ = "medication_issue_record"
     _pk = mapped_column(t.Integer, primary_key=True)
 
-    patient_id = mapped_column(t.VARBINARY())
-    dmd_product_code_id = mapped_column(t.BIGINT())
+    patient_id = mapped_column(t.VARBINARY)
+    dmd_product_code_id = mapped_column(t.BIGINT)
     effective_datetime = mapped_column(trdt.TIMESTAMP(precision=6, timezone=True))
 
 
@@ -20,17 +20,17 @@ class Observation(Base):
     __tablename__ = "observation"
     _pk = mapped_column(t.Integer, primary_key=True)
 
-    patient_id = mapped_column(t.VARBINARY())
+    patient_id = mapped_column(t.VARBINARY)
     effective_datetime = mapped_column(trdt.TIMESTAMP(precision=6, timezone=True))
     numeric_value = mapped_column(t.DECIMAL(precision=19, scale=3))
-    snomed_concept_id = mapped_column(t.BIGINT())
+    snomed_concept_id = mapped_column(t.BIGINT)
 
 
 class Patient(Base):
     __tablename__ = "patient"
     _pk = mapped_column(t.Integer, primary_key=True)
 
-    patient_id = mapped_column(t.VARBINARY())
+    patient_id = mapped_column(t.VARBINARY)
     date_of_birth = mapped_column(trdt.TIMESTAMP(precision=6))
     date_of_death = mapped_column(trdt.TIMESTAMP(precision=6))
-    sex = mapped_column(t.VARCHAR())
+    sex = mapped_column(t.VARCHAR)
