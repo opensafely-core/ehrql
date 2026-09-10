16e3427b631799454d59b34d28160eeb75c2da27
Dave Evans <dave.evans@thedatalab.org>
2026-09-10 17:31:06 +0100

    Add test case exposing bug in rewrite logic
    Note that this is a worst case example for this particular bug as it
    produces a valid query which is not the one intended. After an
    exhaustive search we can confirm that the only cases which hit this bug
    in the wild involve generating invalid query model and blowing up before
    any SQL is executed.

--- tests/unit/query_model/test_query_graph_rewriter.py
+++ tests/unit/query_model/test_query_graph_rewriter.py
@@ -1,4 +1,5 @@
 from ehrql.query_model.nodes import (
+    AggregateByPatient,
     Case,
     Column,
     Filter,
@@ -81,3 +82,60 @@ def test_query_graph_rewriter_handles_replacing_node_with_value():
     new_graph = rewriter.rewrite(graph)
 
     assert new_graph == {"i": Value(10), "s": Value("a")}
+
+
+def test_query_graph_rewriter_edge_case():
+    # We construct a simple graph containing two tables. The first table is filtered
+    # using a value obtained by aggregating over that table itself. The second table is
+    # only there to prevent some short-circuiting logic from kicking in and masking the
+    # bug.
+    def make_graph(table_1, table_2):
+        table_1_i = SelectColumn(source=table_1, name="i")
+        return {
+            "t1": Filter(
+                source=table_1,
+                condition=Function.EQ(
+                    lhs=table_1_i,
+                    # This is the construct which triggers the bug. While `table_1` is
+                    # being rewritten there is a temporary period in which the node
+                    # cache contains incorrect values. The expression below gets
+                    # rewritten using the incorrect values which means that it doesn't
+                    # get the appropriate replacements applied
+                    rhs=AggregateByPatient.Max(table_1_i),
+                ),
+            ),
+            "t2": table_2,
+        }
+
+    # This takes a table and returns that table with a simple filter applied
+    def make_filtered_table(table):
+        return Filter(
+            source=table,
+            condition=Function.LE(
+                lhs=SelectColumn(source=table, name="i"),
+                rhs=Value(2000),
+            ),
+        )
+
+    schema = TableSchema(i=Column(int))
+    table_1_orig = SelectTable(name="table_1", schema=schema)
+    table_2_orig = SelectTable(name="table_2", schema=schema)
+
+    # We start by constructing a graph using two base tables
+    example = make_graph(table_1_orig, table_2_orig)
+
+    # We then create filtered versions of those tables and construct a new graph using
+    # those filtered tables
+    table_1_filtered = make_filtered_table(table_1_orig)
+    table_2_filtered = make_filtered_table(table_2_orig)
+    expected = make_graph(table_1_filtered, table_2_filtered)
+
+    # This is exactly the graph we should expect if we take the original graph and
+    # replace the table references with filtered tables. However due to a bug, this
+    # fails.
+    rewriter = QueryGraphRewriter()
+    rewriter.replace(table_1_orig, table_1_filtered)
+    rewriter.replace(table_2_orig, table_2_filtered)
+    new_graph = rewriter.rewrite(example)
+
+    assert new_graph == expected
